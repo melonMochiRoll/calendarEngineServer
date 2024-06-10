eb747ee0b8ff1e3d69be12fdd42e71b50279aaa8
melonMochiRoll <soulhwasin@gmail.com>
2024-06-10 22:50:23 +0900

    :fire:

--- client/src/api/axiosInstance.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const DEVELOPMENT_BASEURL = 'http://localhost:3000';
-
-export const axiosInstance = axios.create({
-  baseURL: DEVELOPMENT_BASEURL,
-  withCredentials: true,
-});
