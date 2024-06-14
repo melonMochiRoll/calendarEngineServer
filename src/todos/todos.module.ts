58e757563e0e8aea003a36476cc4af4ec361a4b9
melonMochiRoll <soulhwasin@gmail.com>
2024-06-14 19:29:12 +0900

    :heavy_plus_sign: CacheManagerModule 불러오기

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -3,9 +3,13 @@ import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todos } from 'src/entities/Todos';
+import { CacheManagerModule } from 'src/cacheManager/cacheManager.module';
 
 @Module({
-  imports: [ TypeOrmModule.forFeature([Todos]) ],
+  imports: [
+    TypeOrmModule.forFeature([Todos]),
+    CacheManagerModule,
+  ],
   controllers: [ TodosController ],
   providers: [ TodosService ],
 })
