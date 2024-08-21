5278e5c566087bec298dfb31c6c8c456b62e5632
melonMochiRoll <soulhwasin@gmail.com>
2024-08-21 23:59:59 +0900

    :card_file_box: 전체적인 컬럼 수정

--- src/entities/Users.ts
+++ src/entities/Users.ts
@@ -1,42 +1,46 @@
-import { IsEmail, IsString } from "class-validator";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, Index, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 import { Todos } from "./Todos";
+import { Sharedspaces } from "./Sharedspaces";
+import { SharedspaceMembers } from "./SharedspacesMembers";
 
-@Entity()
+@Unique('users_email_idx', ['email'])
+@Entity({ name: 'users' })
 export class Users {
-
-  @PrimaryGeneratedColumn({
-    type: 'int',
-    unsigned: true,
-  })
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
   id: number;
 
-  @IsEmail()
-  @Column({
-    type: 'varchar',
-    length: 100,
-  })
+  @Column({ type: 'varchar', name: 'email', length: 30 })
   email: string;
 
-  @IsString()
-  @Column({
-    type: 'varchar',
-    length: 100,
-  })
+  @Column({ type: 'varchar', length: 100, select: false })
   password: string;
 
-  @Column({
-    type: 'date',
-    default: () => '(CURRENT_DATE)',
-  })
+  @CreateDateColumn()
   createdAt: Date;
 
-  @Column({
-    type: 'date',
-    nullable: true,
-  })
-  deletedAt: Date;
+  @DeleteDateColumn()
+  deletedAt: Date | null;
 
-  @OneToMany(() => Todos, todos => todos.User)
+  @OneToMany(() => Todos, todos => todos.Author)
   Todos: Todos[];
+
+  @OneToMany(() => Sharedspaces, sharedspaces => sharedspaces.Owner)
+  OwnedSharedspaces: Sharedspaces[];
+
+  @OneToMany(() => SharedspaceMembers, sharedspacemembers => sharedspacemembers.User)
+  Sharedspacemembers: SharedspaceMembers[];
+
+  @ManyToMany(() => Sharedspaces, sharedspaces => sharedspaces.Members)
+  @JoinTable({
+    name: 'sharedspacesmembers',
+    joinColumn: {
+      name: 'UserId',
+      referencedColumnName: 'id'
+    },
+    inverseJoinColumn: {
+      name: 'SharedspaceId',
+      referencedColumnName: 'id'
+    }
+  })
+  Sharedspaces: Sharedspaces[];
 }
