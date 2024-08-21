59d563a19cc3d966904459874e46be34cf280eea
melonMochiRoll <soulhwasin@gmail.com>
2024-08-21 23:59:31 +0900

    :card_file_box: Sharedspaces 엔티티 정의

---
+++ src/entities/Sharedspaces.ts
@@ -0,0 +1,51 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, Index, JoinColumn, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Users } from "./Users";
+import { Todos } from "./Todos";
+import { SharedspaceMembers } from "./SharedspacesMembers";
+
+@Index('sharedspace_OwnerId_idx', ['OwnerId'])
+@Unique('sharedspace_name_idx', ['name'])
+@Unique('sharedspace_url_idx', ['url'])
+@Entity({ name: 'sharedspaces' })
+export class Sharedspaces {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column({ type: 'varchar', name: 'name', length: 30 })
+  name: string;
+
+  @Column({ type: 'varchar', name: 'url', length: 50 })
+  url: string;
+
+  @Column({ type: 'boolean', default: 1 })
+  private: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+  
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+
+  @Column({ type: 'int', name: 'OwnerId', nullable: true })
+  OwnerId: number | null;
+
+  @ManyToOne(() => Users, users => users.OwnedSharedspaces, {
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({
+    name: 'OwnerId',
+    referencedColumnName: 'id',
+    foreignKeyConstraintName: 'sharedspaces_OwnerId_fk'
+  })
+  Owner: Users;
+
+  @OneToMany(() => Todos, todos => todos.Sharedspace)
+  Todos: Todos[];
+
+  @OneToMany(() => SharedspaceMembers, sharedspaceMembers => sharedspaceMembers.Sharedspace)
+  Sharedspacemembers: SharedspaceMembers[];
+
+  @ManyToMany(() => Users, users => users.Sharedspaces)
+  Members: Users[];
+}
