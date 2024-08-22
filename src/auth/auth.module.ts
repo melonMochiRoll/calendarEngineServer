e91453973f9b765efea5b5670485bd89adf0e610
melonMochiRoll <soulhwasin@gmail.com>
2024-08-22 17:26:10 +0900

    :heavy_plus_sign: userRepository 불러오기

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,13 @@ import { AuthController } from './auth.controller';
 import { LocalStrategy } from './local.strategy';
 import { SessionSerializer } from './session.serializer';
 import { CacheManagerModule } from 'src/cacheManager/cacheManager.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
 
 @Module({
   imports: [
     PassportModule.register({ session: true }),
+    TypeOrmModule.forFeature([Users]),
     UsersModule,
     CacheManagerModule,
   ],
