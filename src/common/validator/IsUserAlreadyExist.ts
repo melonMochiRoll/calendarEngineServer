8da1e7af7db68c653f2f4dfaff1aa646e544f779
melonMochiRoll <soulhwasin@gmail.com>
2024-08-29 15:57:35 +0900

    :safety_vest: 이미 존재하는 유저인지 검증하는 Validator 정의

---
+++ src/common/validator/IsUserAlreadyExist.ts
@@ -0,0 +1,42 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+
+@ValidatorConstraint({ name: 'isUserAlreadyExist', async: true })
+@Injectable()
+export class IsUserAlreadyExistConstraint implements ValidatorConstraintInterface {
+  constructor(
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {}
+
+  async validate(id: any, args: ValidationArguments) {
+    return await this.usersRepository.findOneByOrFail({ id })
+      .then(() => {
+        return true;
+      })
+      .catch(() => {
+        return false;
+      });
+  }
+}
+
+export function IsUserAlreadyExist(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsUserAlreadyExistConstraint,
+    });
+  };
+}
