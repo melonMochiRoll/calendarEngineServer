4911afcb06d9aa8ae96db68cdb0631a88126f086
melonMochiRoll <soulhwasin@gmail.com>
2024-08-29 15:58:53 +0900

    :safety_vest: validator 적용

--- src/todos/dto/create.todo.dto.ts
+++ src/todos/dto/create.todo.dto.ts
@@ -1,25 +1,44 @@
-import { IsDate, IsNotEmpty, IsNumber, MaxLength } from "class-validator";
+import { IsDateString, IsNotEmpty, IsNumber, MaxLength } from "class-validator";
+import { IsSpaceAlreadyExist } from "src/common/validator/IsSpaceAlreadyExist";
+import { IsTimeFormat } from "src/common/validator/IsTimeFormat";
+import { IsUserAlreadyExist } from "src/common/validator/IsUserAlreadyExist";
 
-export class CreateTodoDTO { // TODO : 커스텀 데코데이터 적용해보기
+export class CreateTodoDTO {
 
-  @MaxLength(1000)
+  @MaxLength(1000, {
+    message: '$property의 길이는 $constraint1자를 넘을 수 없습니다.',
+  })
   @IsNotEmpty()
   description: string;
 
-  @IsDate()
+  @IsDateString({ strict: true }, {
+    message: '$property는 Date형식이 아닙니다.'
+  })
   @IsNotEmpty()
   date: Date;
 
+  @IsTimeFormat({
+    message: '$property은 Time형식이 아닙니다.'
+  })
   @IsNotEmpty()
   startTime: string;
 
+  @IsTimeFormat({
+    message: '$property은 Time형식이 아닙니다.'
+  })
   @IsNotEmpty()
   endTime: string;
 
+  @IsUserAlreadyExist({
+    message: '$property은 존재하지 않는 회원입니다.'
+  })
   @IsNumber()
   @IsNotEmpty()
   AuthorId: number;
 
+  @IsSpaceAlreadyExist({
+    message: '$property은 존재하지 않는 스페이스입니다.'
+  })
   @IsNumber()
   @IsNotEmpty()
   SharedspaceId: number;
