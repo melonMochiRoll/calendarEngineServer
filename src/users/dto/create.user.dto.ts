37021c02615bf2fcccf1f387344e049feb877468
melonMochiRoll <soulhwasin@gmail.com>
2024-08-30 18:54:05 +0900

    :safety_vest: 이메일 중복 여부 validator 추가

--- src/users/dto/create.user.dto.ts
+++ src/users/dto/create.user.dto.ts
@@ -1,7 +1,11 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { IsEmailAlreadyExist } from "src/common/validator/IsEmailAlreadyExist";
 
 export class CreateUserDTO {
 
+  @IsEmailAlreadyExist({
+    message: '이미 존재하는 $property입니다.',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
