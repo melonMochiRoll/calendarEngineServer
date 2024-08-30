7237baaa602c4335330b89f80e5485ffe1171f1f
melonMochiRoll <soulhwasin@gmail.com>
2024-08-30 21:27:17 +0900

    :heavy_plus_sign: IsTodoAlreadyExistConstraint providers 추가

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -3,11 +3,15 @@ import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todos } from 'src/entities/Todos';
+import { IsTodoAlreadyExistConstraint } from 'src/common/validator/IsTodoAlreadyExist';
 
 @Module({
   imports: [ TypeOrmModule.forFeature([Todos]) ],
   controllers: [ TodosController ],
-  providers: [ TodosService ],
+  providers: [
+    TodosService,
+    IsTodoAlreadyExistConstraint,
+  ],
 })
 
 export class TodosModule {}
