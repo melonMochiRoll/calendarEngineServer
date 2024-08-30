a71b1943a1e5dd23264d4c661fb19636ab063fec
melonMochiRoll <soulhwasin@gmail.com>
2024-08-30 16:39:07 +0900

    :heavy_plus_sign: IsUserAlreadyExistConstraint IsUserAlreadyExistConstraint providers 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,15 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { Users } from 'src/entities/Users';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { IsUserAlreadyExistConstraint } from 'src/common/validator/IsUserAlreadyExist';
 
 @Module({
   imports: [ TypeOrmModule.forFeature([Users]) ],
   controllers: [ UsersController ],
-  providers: [ UsersService ],
+  providers: [
+    UsersService,
+    IsUserAlreadyExistConstraint,
+  ],
   exports: [ UsersService ],
 })
 
