99596970fbf77d10357c68ba219addd33abdf308
melonMochiRoll <soulhwasin@gmail.com>
2024-09-02 19:17:04 +0900

    :sparkles: roles를 검증하는 가드 정의

---
+++ src/common/guard/roles.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Roles } from '../decorator/roles.decorator';
+import { SharedspaceMembers } from 'src/entities/SharedspaceMembers';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  
+  canActivate(context: ExecutionContext) {
+    const roles = this.reflector.get(Roles, context.getHandler());
+    const userSpaces = context.switchToHttp().getRequest().user['Sharedspacemembers'];
+    const spaceId = context.switchToHttp().getRequest().body['SharedspaceId'];
+
+    const userRoles = userSpaces
+      .filter((item: SharedspaceMembers) => item.SharedspaceId === spaceId)
+      .map((item: SharedspaceMembers) => item?.role || '');
+
+    if (!matchRoles(roles, userRoles)) {
+      throw new ForbiddenException(`접근 권한이 없습니다`);
+    }
+
+    return true;
+  }
+}
+
+const matchRoles = (roles: string[], userRoles: string[]) => {
+  return roles.reduce((acc: boolean, role: string) => {
+    if (!acc && userRoles.includes(role)) {
+      return true;
+    } else {
+      return acc;
+    }
+  }, false);
+};
