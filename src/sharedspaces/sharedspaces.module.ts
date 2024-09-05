66f94c90e446585d5b1ab592f435dbd7dd999a38
melonMochiRoll <soulhwasin@gmail.com>
2024-09-05 14:53:05 +0900

    :heavy_plus_sign: SharedspaceMembers Repository DI를 위한 불러오기

--- src/sharedspaces/sharedspaces.module.ts
+++ src/sharedspaces/sharedspaces.module.ts
@@ -4,9 +4,15 @@ import { Sharedspaces } from 'src/entities/Sharedspaces';
 import { SharedspacesController } from './sharedspaces.controller';
 import { SharedspacesService } from './sharedspaces.service';
 import { IsExistSpaceConstraint } from 'src/common/validator/IsExistSpace';
+import { SharedspaceMembers } from 'src/entities/SharedspaceMembers';
 
 @Module({
-  imports: [ TypeOrmModule.forFeature([Sharedspaces]) ],
+  imports: [
+    TypeOrmModule.forFeature([
+      Sharedspaces,
+      SharedspaceMembers,
+    ])
+  ],
   controllers: [ SharedspacesController ],
   providers: [
     SharedspacesService,
