245af7d392327758f963698722e7685e6757e542
melonMochiRoll <soulhwasin@gmail.com>
2024-09-05 15:32:21 +0900

    :label: 스페이스 등급 enum 추가

--- src/typings/types.ts
+++ src/typings/types.ts
@@ -1,9 +1,15 @@
 import { Todos } from "src/entities/Todos";
 
-export type TodosWithoutUserId = Omit<Todos, 'AuthorId'>;
+export type TodosWithoutUserId = Omit<Todos, 'AuthorId'>; // 수정 필요
 
-type ProcessedTodo = Pick<Todos, 'id' | 'description' | 'isComplete'>;
+type ProcessedTodo = Pick<Todos, 'id' | 'description'>; // 테이블 구조 변경으로 인한 수정 필요
 
 export type ProcessedTodos = {
   [key: string]: ProcessedTodo[],
+};
+
+export const enum ESharedspaceMembersRoles {
+  OWNER = 'owner',
+  MEMBER = 'member',
+  VIEWER = 'viewer',
 };
