404cf266e79229aa553740214927ed1a6d4ccf92
melonMochiRoll <soulhwasin@gmail.com>
2024-09-06 19:50:08 +0900

    :sparkles: CRUD 구현 및 인증, 역할 검증 가드 적용

--- src/sharedspaces/sharedspaces.controller.ts
+++ src/sharedspaces/sharedspaces.controller.ts
@@ -1,6 +1,41 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Patch, Post, UseGuards } from "@nestjs/common";
+import { SharedspacesService } from "./sharedspaces.service";
+import { CreateSharedspaceDTO } from "./dto/create.sharedspace.dto";
+import { IsAuthenicatedGuard } from "src/auth/local.auth.guard";
+import { UpdateSharedspaceNameDTO } from "./dto/update.sharedspace.name.dto";
+import { UpdateSharedspaceOwnerDTO } from "./dto/update.sharedspace.owner.dto";
+import { DeleteSharedspaceDTO } from "./dto/delete.sharedspace.dto";
+import { RolesGuard } from "src/common/guard/roles.guard";
+import { OwnerOnlyRoles } from "src/common/decorator/owner.only.decorator";
 
+@UseGuards(IsAuthenicatedGuard)
+@UseGuards(RolesGuard)
 @Controller('api/sharedspaces')
 export class SharedspacesController {
-  constructor() {}
+  constructor(
+    private sharedspacesService: SharedspacesService,
+  ) {}
+
+  @Post()
+  createSharedspace(@Body() dto: CreateSharedspaceDTO) {
+    return this.sharedspacesService.createSharedspace(dto);
+  }
+
+  @OwnerOnlyRoles()
+  @Patch('name')
+  updateSharedspaceName(@Body() dto: UpdateSharedspaceNameDTO) {
+    return this.sharedspacesService.updateSharedspaceName(dto);
+  }
+
+  @OwnerOnlyRoles()
+  @Patch('owner')
+  updateSharedspaceOwner(@Body() dto: UpdateSharedspaceOwnerDTO) {
+    return this.sharedspacesService.updateSharedspaceOwner(dto);
+  }
+  
+  @OwnerOnlyRoles()
+  @Delete()
+  deleteSharedspace(@Body() dto: DeleteSharedspaceDTO) {
+    return this.sharedspacesService.deleteSharedspace(dto);
+  }
 }
--- src/sharedspaces/sharedspaces.service.ts
+++ src/sharedspaces/sharedspaces.service.ts
@@ -1,6 +1,119 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { CreateSharedspaceDTO } from "./dto/create.sharedspace.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DataSource, Repository } from "typeorm";
+import { Sharedspaces } from "src/entities/Sharedspaces";
+import { nanoid } from "nanoid";
+import { UpdateSharedspaceNameDTO } from "./dto/update.sharedspace.name.dto";
+import { UpdateSharedspaceOwnerDTO } from "./dto/update.sharedspace.owner.dto";
+import { DeleteSharedspaceDTO } from "./dto/delete.sharedspace.dto";
+import { SharedspaceMembers } from "src/entities/SharedspaceMembers";
+import { ESharedspaceMembersRoles } from "src/typings/types";
 
 @Injectable()
 export class SharedspacesService {
-  constructor() {}
+  constructor(
+    private dataSource: DataSource,
+    @InjectRepository(Sharedspaces)
+    private sharedspacesRepository: Repository<Sharedspaces>,
+    @InjectRepository(SharedspaceMembers)
+    private sharedspaceMembersRepository: Repository<SharedspaceMembers>,
+  ) {}
+
+  async createSharedspace(dto: CreateSharedspaceDTO) {
+    const { OwnerId } = dto;
+
+    const qr = this.dataSource.createQueryRunner();
+    await qr.connect();
+    await qr.startTransaction();
+
+    try {
+      const created = await qr.manager.save(Sharedspaces, {
+        url: nanoid(5),
+        ...dto,
+      });
+      
+      await qr.manager.save(SharedspaceMembers, {
+        UserId: OwnerId,
+        SharedspaceId: created.id,
+        role: ESharedspaceMembersRoles.OWNER,
+      });
+
+      await qr.commitTransaction();
+    } catch (err) {
+      console.error(`createSharedspace : ${err}`);
+
+      await qr.rollbackTransaction();
+
+      throw new InternalServerErrorException(err);
+    } finally {
+      await qr.release();
+    }
+
+    return true;
+  }
+
+  async updateSharedspaceName(dto: UpdateSharedspaceNameDTO) {
+    const { name, SharedspaceId } = dto;
+    
+    try {
+      const origin = await this.sharedspacesRepository.findOneBy({ id: SharedspaceId });
+
+      if (origin.name === name) {
+        throw new ConflictException('동일한 이름으로 바꿀수 없습니다.');
+      }
+
+      await this.sharedspacesRepository.update({ id: SharedspaceId }, { name });
+    } catch (err) {
+      console.error(`updateSharedspaceName : ${err}`);
+      throw new InternalServerErrorException(err);
+    }
+
+    return true;
+  }
+
+  async updateSharedspaceOwner(dto: UpdateSharedspaceOwnerDTO) {
+    const { OwnerId, newOwnerId, SharedspaceId } = dto;
+
+    const qr = this.dataSource.createQueryRunner();
+    await qr.connect();
+    await qr.startTransaction();
+
+    try {
+      const origin = await this.sharedspacesRepository.findOneBy({ id: SharedspaceId });
+
+      if (origin.OwnerId === newOwnerId) {
+        throw new ConflictException('동일한 유저로 바꿀수 없습니다.');
+      }
+
+      await qr.manager.update(Sharedspaces, { id: SharedspaceId }, { OwnerId: newOwnerId });
+      await qr.manager.update(SharedspaceMembers, { UserId: OwnerId, SharedspaceId }, { role: ESharedspaceMembersRoles.MEMBER });
+      await qr.manager.save(SharedspaceMembers, { UserId: newOwnerId, SharedspaceId, role: ESharedspaceMembersRoles.OWNER });
+
+      await qr.commitTransaction();
+    } catch (err) {
+      console.error(`updateSharedspaceOwner : ${err}`);
+
+      await qr.rollbackTransaction();
+
+      throw new InternalServerErrorException(err);
+    } finally {
+      await qr.release();
+    }
+
+    return true;
+  }
+
+  async deleteSharedspace(dto: DeleteSharedspaceDTO) {
+    const { SharedspaceId } = dto;
+
+    try {
+      await this.sharedspacesRepository.delete({ id: SharedspaceId });
+    } catch (err) {
+      console.error(`deleteSharedspace : ${err}`);
+      throw new InternalServerErrorException(err);
+    }
+
+    return true;
+  }
 }
