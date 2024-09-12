8d58285ccc30015a87313f2804e5a04b51e49121
melonMochiRoll <soulhwasin@gmail.com>
2024-09-12 15:31:31 +0900

    :heavy_plus_sign: TodosModule 의존성 추가

--- src/sharedspaces/sharedspaces.module.ts
+++ src/sharedspaces/sharedspaces.module.ts
@@ -5,13 +5,15 @@ import { SharedspacesController } from './sharedspaces.controller';
 import { SharedspacesService } from './sharedspaces.service';
 import { IsExistSpaceConstraint } from 'src/common/validator/IsExistSpace';
 import { SharedspaceMembers } from 'src/entities/SharedspaceMembers';
+import { TodosModule } from 'src/todos/todos.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Sharedspaces,
       SharedspaceMembers,
-    ])
+    ]),
+    TodosModule,
   ],
   controllers: [ SharedspacesController ],
   providers: [
