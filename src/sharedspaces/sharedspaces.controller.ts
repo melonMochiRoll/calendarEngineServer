301862d641cbe7cfae9aebf1c3c59e36f477320a
melonMochiRoll <soulhwasin@gmail.com>
2024-09-12 14:57:14 +0900

    :art: 컨트롤러 전체 가드에서 메서드 별 가드로 수정

--- src/sharedspaces/sharedspaces.controller.ts
+++ src/sharedspaces/sharedspaces.controller.ts
@@ -1,45 +1,61 @@
-import { Body, Controller, Delete, Get, Patch, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from "@nestjs/common";
 import { SharedspacesService } from "./sharedspaces.service";
 import { CreateSharedspaceDTO } from "./dto/create.sharedspace.dto";
-import { IsAuthenicatedGuard } from "src/auth/local.auth.guard";
 import { UpdateSharedspaceNameDTO } from "./dto/update.sharedspace.name.dto";
 import { UpdateSharedspaceOwnerDTO } from "./dto/update.sharedspace.owner.dto";
 import { DeleteSharedspaceDTO } from "./dto/delete.sharedspace.dto";
-import { RolesGuard } from "src/common/guard/roles.guard";
 import { OwnerOnlyRoles } from "src/common/decorator/owner.only.decorator";
 import { User } from "src/common/decorator/user.decorator";
 import { Users } from "src/entities/Users";
+import { AuthRoleGuards } from "src/common/decorator/auth.role.decorator";
+import { IsAuthenicatedGuard } from "src/auth/local.auth.guard";
 
-@UseGuards(IsAuthenicatedGuard)
-@UseGuards(RolesGuard)
 @Controller('api/sharedspaces')
 export class SharedspacesController {
   constructor(
     private sharedspacesService: SharedspacesService,
   ) {}
 
+  @UseGuards(IsAuthenicatedGuard)
   @Get()
   getSharedspaces(@User() user: Users) {
     return this.sharedspacesService.getSharedspaces(user);
   }
 
+  @Get(':url/todos')
+  getTodosForSpace(
+    @Param('url') url: string,
+    @Query('date') date: string,
+    @User() user: Users,
+  ) {
+    return this.sharedspacesService.getTodosForSpace(
+      url,
+      date,
+      user,
+    );
+  };
+
+  @UseGuards(IsAuthenicatedGuard)
   @Post()
   createSharedspace(@Body() dto: CreateSharedspaceDTO) {
     return this.sharedspacesService.createSharedspace(dto);
   }
 
+  @AuthRoleGuards()
   @OwnerOnlyRoles()
   @Patch('name')
   updateSharedspaceName(@Body() dto: UpdateSharedspaceNameDTO) {
     return this.sharedspacesService.updateSharedspaceName(dto);
   }
 
+  @AuthRoleGuards()
   @OwnerOnlyRoles()
   @Patch('owner')
   updateSharedspaceOwner(@Body() dto: UpdateSharedspaceOwnerDTO) {
     return this.sharedspacesService.updateSharedspaceOwner(dto);
   }
   
+  @AuthRoleGuards()
   @OwnerOnlyRoles()
   @Delete()
   deleteSharedspace(@Body() dto: DeleteSharedspaceDTO) {
