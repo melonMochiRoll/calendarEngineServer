0dfbdb39b23aad7724bad5ea3362ca11337bfc56
melonMochiRoll <soulhwasin@gmail.com>
2024-09-12 17:39:26 +0900

    :sparkles: INTERNAL_SERVER_MESSAGE 정의 및 주석 추가

--- src/common/constant/errorMessages.ts
+++ src/common/constant/errorMessages.ts
@@ -1,6 +1,8 @@
 
-export const UNAUTHORIZED_MESSAGE = '인증이 필요합니다.';
+export const UNAUTHORIZED_MESSAGE = '인증이 필요합니다.'; // status: 401
 
-export const NOT_FOUND_SPACE_MESSAGE = '존재하지 않는 스페이스입니다.';
+export const NOT_FOUND_SPACE_MESSAGE = '존재하지 않는 스페이스입니다.'; // status: 404
 
-export const ACCESS_DENIED_MESSAGE = '접근 권한이 없습니다.';
+export const ACCESS_DENIED_MESSAGE = '접근 권한이 없습니다.'; // status: 403
+
+export const INTERNAL_SERVER_MESSAGE = '서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.'; // status: 500
