d5879cf7fbecb394a47956a118cc65094afb5a8f
melonMochiRoll <soulhwasin@gmail.com>
2024-09-16 17:23:38 +0900

    :fire: searchTodos 삭제

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,7 +1,5 @@
-import { Body, Controller, Delete, Get, HttpCode, ParseIntPipe, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, HttpCode, ParseIntPipe, Post, Put, Query } from '@nestjs/common';
 import { TodosService } from './todos.service';
-import { User } from 'src/common/decorator/user.decorator';
-import { Users } from 'src/entities/Users';
 import { CreateTodoDTO } from './dto/create.todo.dto';
 import { UpdateTodoDto } from './dto/update.todo.dto';
 import { AboveMemberRoles } from 'src/common/decorator/above.member.decorator';
@@ -18,14 +16,14 @@ export class TodosController {
   @Post()
   createTodo(@Body() dto: CreateTodoDTO) {
     return this.todosService.createTodo(dto);
-  };
+  }
 
   @AuthRoleGuards()
   @AboveMemberRoles()
   @Put()
   updateTodo(@Body() dto: UpdateTodoDto) {
     return this.todosService.updateTodo(dto);
-  };
+  }
 
   @AuthRoleGuards()
   @AboveMemberRoles()
@@ -34,21 +32,4 @@ export class TodosController {
   deleteTodo(@Query('ti', ParseIntPipe) todoId: number) {
     return this.todosService.deleteTodo(todoId);
   }
-
-  // @Get('search')
-  // searchTodos( // TODO: 검색 전략 고민
-  //   @Query('query') query: string,
-  //   @Query('offset', ParseIntPipe) offset: number,
-  //   @Query('limit', ParseIntPipe) limit: number,
-  //   @User() user: Users,
-  // ) {
-  //   // return this.todosService.searchTodos(
-  //   //   query,
-  //   //   offset,
-  //   //   limit,
-  //   //   user.id,
-  //   // );
-  // }
 }
-
-// TODO : 로직 수정에 따른 캐싱 전략 수정 필요
