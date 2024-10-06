7225a0a8f77001a4780ace2a61d08595e41f2b25
melonMochiRoll <soulhwasin@gmail.com>
2024-10-06 18:52:08 +0900

    :recycle: ESharedspaceMembersRole, as const로 수정

--- src/typings/types.ts
+++ src/typings/types.ts
@@ -2,18 +2,19 @@ import { Todos } from "src/entities/Todos";
 
 export type TodosWithoutUserId = Omit<Todos, 'AuthorId'>; // 테이블 구조 변경으로 인한 수정 필요
 
-
 type ProcessedTodo = Pick<Todos, 'id' | 'description'>; // 테이블 구조 변경으로 인한 수정 필요
 
 export type ProcessedTodos = { // 테이블 구조 변경으로 인한 수정 필요
   [key: string]: ProcessedTodo[],
 };
 
-export const enum ESharedspaceMembersRoles {
-  OWNER = 'owner',
-  MEMBER = 'member',
-  VIEWER = 'viewer',
-};
+export const SharedspaceMembersRoles = {
+  OWNER: 'owner',
+  MEMBER: 'member',
+  VIEWER: 'viewer',
+} as const;
+
+export type TSharedspaceMembersRole = typeof SharedspaceMembersRoles[keyof typeof SharedspaceMembersRoles];
 
 export const SubscribedspacesFilter = {
   ALL: 'all',
