7b521555eb413c48efe5f5a3e6e86be5b2f3bcdb
melonMochiRoll <soulhwasin@gmail.com>
2024-10-06 18:53:52 +0900

    :recycle: ESharedspaceMembersRole, as const로 수정

--- src/common/decorator/owner.only.decorator.ts
+++ src/common/decorator/owner.only.decorator.ts
@@ -1,9 +1,9 @@
 import { applyDecorators } from "@nestjs/common";
 import { Roles } from 'src/common/decorator/roles.decorator';
-import { ESharedspaceMembersRoles } from "src/typings/types";
+import { SharedspaceMembersRoles } from "src/typings/types";
 
 export function OwnerOnlyRoles() {
   return applyDecorators(
-    Roles([ESharedspaceMembersRoles.OWNER])
+    Roles([SharedspaceMembersRoles.OWNER])
   );
 }
