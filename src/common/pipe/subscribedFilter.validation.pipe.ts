520de3781417765c03ae1cca74a9ecaa3b80b5a4
melonMochiRoll <soulhwasin@gmail.com>
2024-10-06 18:00:49 +0900

    :sparkles: subscribedFilter 검증 pipe 정의

---
+++ src/common/pipe/subscribedFilter.validation.pipe.ts
@@ -0,0 +1,18 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+import { BAD_REQUEST_MESSAGE } from '../constant/error.message';
+import { SubscribedspacesFilter, TSubscribedspacesFilter } from 'src/typings/types';
+
+@Injectable()
+export class SubscribedFilterValidationPipe implements PipeTransform<TSubscribedspacesFilter> {
+  transform(value: string): string {
+    if (!this.isESubscribedspacesFilter(value)) {
+      throw new BadRequestException(BAD_REQUEST_MESSAGE);
+    }
+
+    return value;
+  }
+
+  private isESubscribedspacesFilter(value: any): value is TSubscribedspacesFilter {
+    return Object.values(SubscribedspacesFilter).includes(value);
+  }
+}
