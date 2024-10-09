56e082d1935537c207cd0a7d3905df89aa9d720e
melonMochiRoll <soulhwasin@gmail.com>
2024-10-09 20:02:37 +0900

    :fire: name 프로퍼티 삭제

--- src/sharedspaces/dto/create.sharedspace.dto.ts
+++ src/sharedspaces/dto/create.sharedspace.dto.ts
@@ -1,17 +1,11 @@
-import { IsNumber, IsString, MaxLength } from "class-validator";
+import { Type } from "class-transformer";
 import { IsExistUser } from "src/common/validator/IsExistUser";
 
 export class CreateSharedspaceDTO {
 
-  @MaxLength(30, {
-    message: '$property의 길이는 $constraint1자를 넘을 수 없습니다.',
-  })
-  @IsString()
-  name: string;
-
   @IsExistUser({
     message: '$property은 존재하지 않는 회원입니다.'
   })
-  @IsNumber()
+  @Type(() => Number)
   OwnerId: number;
 }
