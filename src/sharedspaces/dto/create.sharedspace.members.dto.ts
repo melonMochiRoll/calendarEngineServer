69d2439c6b20c00cab135d4461ef1019e667d989
melonMochiRoll <soulhwasin@gmail.com>
2024-10-21 16:02:24 +0900

    :sparkles: CreateSharedspaceMembersDTO 정의

---
+++ src/sharedspaces/dto/create.sharedspace.members.dto.ts
@@ -0,0 +1,19 @@
+import { Type } from "class-transformer";
+import { IsString } from "class-validator";
+import { IsExistRole } from "src/common/validator/IsExistRole";
+import { IsExistUser } from "src/common/validator/IsExistUser";
+
+export class CreateSharedspaceMembersDTO {
+
+  @IsExistUser({
+    message: '$property는 존재하지 않는 회원입니다.'
+  })
+  @Type(() => Number)
+  UserId: number;
+
+  @IsExistRole({
+    message: '$property는 유효하지 않는 등급입니다.'
+  })
+  @IsString()
+  role: string;
+}
