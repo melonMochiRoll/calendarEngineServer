97fce2ebbf42188b8f763971788deb42e92dcca9
melonMochiRoll <soulhwasin@gmail.com>
2024-10-23 15:53:53 +0900

    :fire: 더 이상 사용하지 않는 타입 제거

--- src/typings/types.ts
+++ src/typings/types.ts
@@ -1,12 +1,3 @@
-import { Todos } from "src/entities/Todos";
-
-export type TodosWithoutUserId = Omit<Todos, 'AuthorId'>; // 테이블 구조 변경으로 인한 수정 필요
-
-type ProcessedTodo = Pick<Todos, 'id' | 'description'>; // 테이블 구조 변경으로 인한 수정 필요
-
-export type ProcessedTodos = { // 테이블 구조 변경으로 인한 수정 필요
-  [key: string]: ProcessedTodo[],
-};
 
 export const SharedspaceMembersRoles = {
   OWNER: 'owner',
