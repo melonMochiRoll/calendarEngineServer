60d502c16acd23d35e2fb7579a632c25d359960c
melonMochiRoll <soulhwasin@gmail.com>
2024-10-24 20:49:13 +0900

    :sparkles: IsExistEmail 정의

---
+++ src/common/validator/IsExistEmail.ts
@@ -0,0 +1,38 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+
+@ValidatorConstraint({ name: 'isExistEmail', async: true })
+@Injectable()
+export class IsExistEmailConstraint implements ValidatorConstraintInterface {
+  constructor(
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {}
+
+  async validate(email: any, args: ValidationArguments) {
+    return await this.usersRepository.findOneByOrFail({ email })
+      .then(() => true)
+      .catch(() => false);
+  }
+}
+
+export function IsExistEmail(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsExistEmailConstraint,
+    });
+  };
+}
