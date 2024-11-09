6474845ad9b49ba03543d9e7397842326460ee77
melonMochiRoll <soulhwasin@gmail.com>
2024-11-09 16:25:38 +0900

    :sparkles: roles 정의

---
+++ src/entities/Roles.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { SharedspaceMembers } from "./SharedspaceMembers";
+import { JoinRequests } from "./JoinRequests";
+
+@Entity({ name: 'roles' })
+export class Roles {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column({ type: 'varchar', name: 'name', length: 100, unique: true })
+  name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => SharedspaceMembers, sharedspaceMembers => sharedspaceMembers.Role)
+  SharedspaceMembers: SharedspaceMembers[];
+
+  @OneToMany(() => JoinRequests, joinRequests => joinRequests.Role)
+  JoinRequests: JoinRequests[];
+}
