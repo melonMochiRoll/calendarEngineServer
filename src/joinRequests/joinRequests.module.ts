3014370153d9f770a1eaa69fb8af312ee80c4343
melonMochiRoll <soulhwasin@gmail.com>
2024-11-10 16:39:36 +0900

    :heavy_plus_sign: joinRequests repository 의존성 추가

--- src/joinRequests/joinRequests.module.ts
+++ src/joinRequests/joinRequests.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JoinRequestsController } from './joinRequests.controller';
 import { JoinRequestsService } from './joinRequests.service';
+import { JoinRequests } from 'src/entities/JoinRequests';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([
+      JoinRequests,
+    ]),
+  ],
   controllers: [ JoinRequestsController ],
   providers: [ JoinRequestsService ],
   exports: [],
