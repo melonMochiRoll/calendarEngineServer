d0ab79cb31ddef5ff2b77f3e82c6c79b1d214fe3
melonMochiRoll <soulhwasin@gmail.com>
2024-11-11 20:06:50 +0900

    :sparkles: TransformSpacePipe 정의

---
+++ src/common/pipe/transform.space.pipe.ts
@@ -0,0 +1,23 @@
+import { ArgumentMetadata, Injectable, NotFoundException, PipeTransform } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Sharedspaces } from "src/entities/Sharedspaces";
+import { IsNull, Repository } from "typeorm";
+import { NOT_FOUND_SPACE_MESSAGE } from "../constant/error.message";
+
+@Injectable()
+export class TransformSpacePipe implements PipeTransform<string> {
+  constructor(
+    @InjectRepository(Sharedspaces)
+    private sharedspacesRepository: Repository<Sharedspaces>,
+  ) {}
+
+  async transform(url: string) {
+    const targetSpace = await this.sharedspacesRepository.findOneBy({ deletedAt: IsNull(), url });
+
+    if (!targetSpace) {
+      throw new NotFoundException(NOT_FOUND_SPACE_MESSAGE);
+    }
+
+    return targetSpace;
+  }
+}
