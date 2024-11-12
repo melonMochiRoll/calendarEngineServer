d34f585c57bbcaf918706c0eb22cf1131551e53d
melonMochiRoll <soulhwasin@gmail.com>
2024-11-12 15:42:23 +0900

    :sparkles: createJoinRequest 정의

--- src/joinRequests/joinRequests.controller.ts
+++ src/joinRequests/joinRequests.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Param, Post, UseGuards } from "@nestjs/common";
+import { IsAuthenicatedGuard } from "src/auth/local.auth.guard";
+import { JoinRequestsService } from "./joinRequests.service";
+import { User } from "src/common/decorator/user.decorator";
+import { Users } from "src/entities/Users";
+import { CreateJoinRequestDTO } from "./dto/create.joinRequest.dto";
+import { TransformSpacePipe } from "src/common/pipe/transform.space.pipe";
+import { Sharedspaces } from "src/entities/Sharedspaces";
 
-@Controller('api/joinrequests')
-export class JoinRequestsController {}
+@Controller('api/sharedspaces')
+export class JoinRequestsController {
+  constructor(
+    private joinRequestsService: JoinRequestsService,
+  ) {}
+
+  @UseGuards(IsAuthenicatedGuard)
+  @Post(':url/joinrequest')
+  createJoinRequest(
+    @Param('url', TransformSpacePipe) targetSpace: Sharedspaces,
+    @Body() dto: CreateJoinRequestDTO,
+    @User() user: Users,
+  ) {
+    return this.joinRequestsService.createJoinRequest(targetSpace, dto, user);
+  }
+}
--- src/joinRequests/joinRequests.service.ts
+++ src/joinRequests/joinRequests.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
+import { Users } from "src/entities/Users";
+import { CreateJoinRequestDTO } from "./dto/create.joinRequest.dto";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { JoinRequests } from "src/entities/JoinRequests";
+import handleError from "src/common/function/handleError";
+import { Sharedspaces } from "src/entities/Sharedspaces";
+import { SharedspaceMembers } from "src/entities/SharedspaceMembers";
+import { CONFLICT_MESSAGE } from "src/common/constant/error.message";
 
 @Injectable()
-export class JoinRequestsService {}
+export class JoinRequestsService {
+  constructor(
+    @InjectRepository(JoinRequests)
+    private joinRequestsRepository: Repository<JoinRequests>,
+    @InjectRepository(SharedspaceMembers)
+    private sharedspaceMembers: Repository<SharedspaceMembers>,
+  ) {}
+  
+  async createJoinRequest(
+    targetSpace: Sharedspaces,
+    dto: CreateJoinRequestDTO,
+    user: Users,
+  ) {
+    try {
+      const isMember = await this.sharedspaceMembers.findOneBy({ UserId: user.id, SharedspaceId: targetSpace.id });
+
+      if (isMember?.RoleName === dto.RoleName) {
+        throw new ConflictException(CONFLICT_MESSAGE);
+      }
+
+      await this.joinRequestsRepository.save({
+        SharedspaceId: targetSpace.id,
+        RequestorId: user.id,
+        RoleName: dto.RoleName,
+      });
+    } catch (err) {
+      handleError(err);
+    }
+
+    return true;
+  }
+}
