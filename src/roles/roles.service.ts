11e27e09ddc0ab3debf5e960fe57dfc24211f1f4
melonMochiRoll <soulhwasin@gmail.com>
2024-11-15 19:59:51 +0900

    :sparkles: service 정의

---
+++ src/roles/roles.service.ts
@@ -0,0 +1,31 @@
+import { ConflictException, Injectable } from "@nestjs/common";
+import handleError from "src/common/function/handleError";
+import { CreateRoleDTO } from "./dto/create.role.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Roles } from "src/entities/Roles";
+import { Repository } from "typeorm";
+import { CONFLICT_MESSAGE } from "src/common/constant/error.message";
+
+@Injectable()
+export class RolesService {
+  constructor(
+    @InjectRepository(Roles)
+    private rolesRepository: Repository<Roles>,
+  ) {}
+  
+  async createRole(dto: CreateRoleDTO) {
+    try {
+      const isExist = this.rolesRepository.findOneBy({ name: dto.name });
+
+      if (isExist) {
+        throw new ConflictException(CONFLICT_MESSAGE);
+      }
+
+      await this.rolesRepository.save({
+        name: dto.name,
+      });
+    } catch (err) {
+      handleError(err);
+    }
+  }
+}
