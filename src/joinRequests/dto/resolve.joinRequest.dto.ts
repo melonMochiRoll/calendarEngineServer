f2e85a212695d5a63965a587437baaf7b05a94ae
melonMochiRoll <soulhwasin@gmail.com>
2024-11-19 17:38:22 +0900

    :sparkles: ResolveJoinRequestDTO 정의

---
+++ src/joinRequests/dto/resolve.joinRequest.dto.ts
@@ -0,0 +1,9 @@
+import { IsExistRole } from "src/common/validator/IsExistRole";
+
+export class ResolveJoinRequestDTO {
+  
+  @IsExistRole({
+    message: '$property는 유효하지 않는 등급입니다.'
+  })
+  RoleName: string;
+}
