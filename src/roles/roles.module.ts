347c7978efe596764d52d89875174cdbd260ec39
melonMochiRoll <soulhwasin@gmail.com>
2024-11-20 15:41:32 +0900

    :heavy_minus_sign: IsExistRoleConstraint 의존성 제거

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { IsExistRoleConstraint } from 'src/common/validator/IsExistRole';
 import { Roles } from 'src/entities/Roles';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
@@ -14,7 +13,6 @@ import { RolesService } from './roles.service';
   controllers: [ RolesController ],
   providers: [
     RolesService,
-    IsExistRoleConstraint,
   ],
   exports: [],
 })
