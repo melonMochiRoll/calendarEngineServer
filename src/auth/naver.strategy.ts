bd73b8f468f44f504d3bb2c945ee3f2af5160726
melonMochiRoll <soulhwasin@gmail.com>
2024-11-26 18:34:30 +0900

    :sparkles: NaverStrategy 정의

---
+++ src/auth/naver.strategy.ts
@@ -0,0 +1,48 @@
+import { ConflictException, Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Strategy } from "passport-naver";
+import { CONFLICT_ACCOUNT_MESSAGE } from "src/common/constant/error.message";
+import handleError from "src/common/function/handleError";
+import { Users } from "src/entities/Users";
+import { ProviderList, TNaverProfile } from "src/typings/types";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class NaverStrategy extends PassportStrategy(Strategy, 'naver') {
+  constructor(
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>
+  ) {
+    super({
+      clientID: process.env.REACT_APP_NAVER_CLIENT_ID,
+      clientSecret: process.env.REACT_APP_NAVER_CLIENT_SECRET,
+      profileURL: 'https://openapi.naver.com/v1/nid/me',
+      authType: 'reprompt',
+    })
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: TNaverProfile) {
+    try {
+      const exUser = await this.usersRepository.findOneBy({ email: profile.emails[0].value });
+
+      if (!exUser) {
+        const newUser = await this.usersRepository.save({
+          email: profile.emails[0].value,
+          profileImage: profile._json.profile_image,
+          provider: ProviderList.NAVER,
+        });
+
+        return newUser;
+      }
+
+      if (exUser.provider !== ProviderList.NAVER) {
+        throw new ConflictException(CONFLICT_ACCOUNT_MESSAGE);
+      }
+
+      return exUser;
+    } catch (err) {
+      handleError(err);
+    }
+  }
+}
