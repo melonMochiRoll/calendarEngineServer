0dd70b21c843bec32feb4ce5ea0165c6b2c282f2
melonMochiRoll <soulhwasin@gmail.com>
2024-11-26 18:36:44 +0900

    :goal_net: validateUser, try catch문 적용

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import { InjectRepository } from "@nestjs/typeorm";
 import { Users } from "src/entities/Users";
 import { Repository } from "typeorm";
+import handleError from "src/common/function/handleError";
 
 @Injectable()
 export class AuthService {
@@ -15,17 +16,21 @@ export class AuthService {
     email: string,
     password: string,
   ) {
-    const user = await this.usersRepository.findOne({
-      where: { email },
-      select: ['id', 'email', 'password'],
-    });
-    const compare = await bcrypt.compare(password, user?.password || '');
-
-    if (!user || !compare) {
-      return null;
+    try {
+      const user = await this.usersRepository.findOne({
+        where: { email },
+        select: ['id', 'email', 'password'],
+      });
+      const compare = await bcrypt.compare(password, user?.password || '');
+  
+      if (!user || !compare) {
+        return null;
+      }
+  
+      const { password: userPassword, ...rest } = user;
+      return rest;
+    } catch (err) {
+      handleError(err);
     }
-
-    const { password: userPassword, ...rest } = user;
-    return rest;
   }
 }
