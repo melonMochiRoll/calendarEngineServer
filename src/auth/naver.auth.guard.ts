90d7fef7e63eef5445bf5619af38f1653ac585c0
melonMochiRoll <soulhwasin@gmail.com>
2024-11-26 16:41:46 +0900

    :sparkles: NaverAuthGuard 정의

---
+++ src/auth/naver.auth.guard.ts
@@ -0,0 +1,16 @@
+import { ExecutionContext, Injectable } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+
+@Injectable()
+export class NaverAuthGuard extends AuthGuard('naver') {
+  async canActivate(context: ExecutionContext) {
+    const result = await super.canActivate(context) as boolean;
+    
+    if (result) {
+      const request = context.switchToHttp().getRequest();
+      await super.logIn(request);
+    }
+
+    return result;
+  }
+}
