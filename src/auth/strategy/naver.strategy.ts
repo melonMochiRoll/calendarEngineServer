5c00ab5822494b08b5181e8d22e931fc10600f47
melonMochiRoll <soulhwasin@gmail.com>
2024-11-29 20:59:43 +0900

    :passport_control: state 검증 로직 추가

--- src/auth/strategy/naver.strategy.ts
+++ src/auth/strategy/naver.strategy.ts
@@ -1,8 +1,9 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import { ConflictException, ForbiddenException, Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { InjectRepository } from "@nestjs/typeorm";
+import { Request } from "express";
 import { Strategy } from "passport-naver";
-import { CONFLICT_ACCOUNT_MESSAGE } from "src/common/constant/error.message";
+import { CONFLICT_ACCOUNT_MESSAGE, MISMATCH_STATE_MESSAGE } from "src/common/constant/error.message";
 import handleError from "src/common/function/handleError";
 import { Users } from "src/entities/Users";
 import { ProviderList, TNaverProfile } from "src/typings/types";
@@ -15,14 +16,26 @@ export class NaverStrategy extends PassportStrategy(Strategy, 'naver') {
     private usersRepository: Repository<Users>
   ) {
     super({
-      clientID: process.env.REACT_APP_NAVER_CLIENT_ID,
-      clientSecret: process.env.REACT_APP_NAVER_CLIENT_SECRET,
+      clientID: process.env.NAVER_CLIENT_ID,
+      clientSecret: process.env.NAVER_CLIENT_SECRET,
       profileURL: 'https://openapi.naver.com/v1/nid/me',
       authType: 'reprompt',
+      passReqToCallback: true,
     })
   }
 
-  async validate(accessToken: string, refreshToken: string, profile: TNaverProfile) {
+  async validate(req: Request, accessToken: string, refreshToken: string, profile: TNaverProfile) {
+    const state = req.session['state'];
+    const providedState = req.query?.state;
+
+    if (state !== providedState) {
+      await fetch(`https://nid.naver.com/oauth2.0/token?client_id=${process.env.NAVER_CLIENT_ID}&client_secret=${process.env.NAVER_CLIENT_SECRET}&access_token=${accessToken}&grant_type=delete`, {
+        method: 'get',
+      });
+      
+      throw new ForbiddenException(MISMATCH_STATE_MESSAGE);
+    }
+
     try {
       const exUser = await this.usersRepository.findOneBy({ email: profile.emails[0].value });
 
