b7e7df10490d1001fde61272d4a4caae1496b422
melonMochiRoll <soulhwasin@gmail.com>
2024-11-29 19:52:37 +0900

    :sparkles: MISMATCH_STATE_MESSAGE 정의

--- src/common/constant/error.message.ts
+++ src/common/constant/error.message.ts
@@ -5,6 +5,8 @@ export const UNAUTHORIZED_MESSAGE = '인증이 필요합니다.'; // status: 401
 
 export const ACCESS_DENIED_MESSAGE = '접근 권한이 없습니다.'; // status: 403
 
+export const MISMATCH_STATE_MESSAGE = 'state 값이 다릅니다.'; // state: 403
+
 export const NOT_FOUND_SPACE_MESSAGE = '존재하지 않는 스페이스입니다.'; // status: 404
 
 export const NOT_FOUND_RESOURCE = '요청하신 리소스를 찾을 수 없습니다.'; // status: 404
