17a21fc3c6d473c83aca7fa2389b0ceb0ad6790b
melonMochiRoll <soulhwasin@gmail.com>
2025-04-09 19:33:53 +0900

    :wrench: mysql 테스팅 모듈 수정

--- src/lib/TypeORMMySqlTestingModule.ts
+++ src/lib/TypeORMMySqlTestingModule.ts
@@ -3,13 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 export const TypeORMMySqlTestingModule = (entities: any[]) =>
   TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'localhost',
-    port: 3307,
-    username: process.env.ORM_USERNAME,
-    password: process.env.ORM_PASSWORD,
-    database: process.env.ORM_DATABASE,
+    host: process.env.DEV_DB_ORIGIN,
+    port: +process.env.DEV_DB_PORT,
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_DATABASE,
     entities: [...entities],
-    synchronize: false,
+    synchronize: true,
+    dropSchema: true,
     autoLoadEntities: true,
     charset: 'utf8mb4',
+    timezone: '+00:00',
   });
