aa873c0accca0e887663292c5a788588570f1130
melonMochiRoll <soulhwasin@gmail.com>
2025-06-16 16:39:55 +0900

    :recycle: RoleName 프로퍼티 제거

--- src/joinRequests/dto/create.joinRequest.dto.ts
+++ src/joinRequests/dto/create.joinRequest.dto.ts
@@ -1,11 +1,4 @@
-import { IsExistRole } from "src/common/validator/IsExistRole";
 
 export class CreateJoinRequestDTO {
-  
-  @IsExistRole({
-    message: '$property는 유효하지 않는 등급입니다.'
-  })
-  RoleName: string;
-
   message: string;
 }
