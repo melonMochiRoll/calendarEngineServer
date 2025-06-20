f903545b78a23c9c5fb621da75a2f22bb44d9425
melonMochiRoll <soulhwasin@gmail.com>
2025-06-20 19:37:33 +0900

    :sparkles: JwtStrategy 정의

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { JwtPayload } from "src/typings/types";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request) => {
+          return request?.cookies['accessToken'] ? request?.cookies['accessToken'] : null;
+        }
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return {
+      email: payload.email,
+      userId: payload.userId,
+    };
+  }
+}
