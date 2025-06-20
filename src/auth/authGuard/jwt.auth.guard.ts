0ab42a5c76e9869a11e6341375719c921c9597dd
melonMochiRoll <soulhwasin@gmail.com>
2025-06-20 19:37:59 +0900

    :sparkles: JwtAuthGuard 정의

---
+++ src/auth/authGuard/jwt.auth.guard.ts
@@ -0,0 +1,9 @@
+import { ExecutionContext, Injectable } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    return await super.canActivate(context) as boolean;
+  }
+}
