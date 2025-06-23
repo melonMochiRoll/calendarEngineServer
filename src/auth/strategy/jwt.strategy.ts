55ce26854b53f4adb96c6c5230c47372f7b2fd21
melonMochiRoll <soulhwasin@gmail.com>
2025-06-23 18:34:39 +0900

    :necktie: JWT 토큰 검증 후 DB에서 유저 데이터를 반환하도록 수정

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,11 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { BAD_REQUEST_MESSAGE } from "src/common/constant/error.message";
+import { Users } from "src/entities/Users";
 import { JwtPayload } from "src/typings/types";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request) => {
@@ -18,9 +25,37 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: JwtPayload) {
-    return {
-      id: payload.id,
-      email: payload.email,
-    };
+    const userData = await this.usersRepository.findOne({
+      select: {
+        id: true,
+        email: true,
+        profileImage: true,
+        Sharedspacemembers: {
+          SharedspaceId: true,
+          Sharedspace: {
+            url: true,
+            private: true,
+          },
+          Role: {
+            name: true,
+          },
+        },
+      },
+      relations: {
+        Sharedspacemembers: {
+          Sharedspace: true,
+          Role: true,
+        },
+      },
+      where: {
+        id: payload.id,
+      },
+    });
+
+    if (!userData) {
+      throw new BadRequestException(BAD_REQUEST_MESSAGE);
+    }
+
+    return userData;
   }
 }
