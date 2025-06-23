a569f0a98a959a93d882dc94636a37c0e4302028
melonMochiRoll <soulhwasin@gmail.com>
2025-06-23 18:25:50 +0900

    :sparkles: IsNotJwtAuthenicatedGuard 정의

--- src/auth/authGuard/jwt.auth.guard.ts
+++ src/auth/authGuard/jwt.auth.guard.ts
@@ -1,9 +1,23 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { ACCESS_DENIED_MESSAGE } from "src/common/constant/error.message";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  async canActivate(context: ExecutionContext): Promise<boolean> {
+  async canActivate(context: ExecutionContext) {
     return await super.canActivate(context) as boolean;
+  }
+}
+
+@Injectable()
+export class IsNotJwtAuthenicatedGuard extends AuthGuard('jwt') {
+  async canActivate(context: ExecutionContext) {
+    const result = await super.canActivate(context);
+
+    if (result) {
+      throw new ForbiddenException(ACCESS_DENIED_MESSAGE);
+    }
+    
+    return true;
   }
 }
