a5c7e192f8310608ee635579a77b1e50d55222d2
melonMochiRoll <soulhwasin@gmail.com>
2025-07-01 20:12:40 +0900

    :sparkles: auth.constants 정의

---
+++ src/common/constant/auth.constants.ts
@@ -0,0 +1,7 @@
+
+export const ACCESS_TOKEN_COOKIE_NAME='accessToken';
+
+export const REFRESH_TOKEN_COOKIE_NAME='refreshToken';
+
+export const CSRF_TOKEN_COOKIE_NAME='csrfToken';
+export const CSRF_TOKEN_HEADER_NAME='X-CSRF-TOKEN';
