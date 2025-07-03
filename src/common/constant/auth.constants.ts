13d85b1e189a3e97d2c9bea8c96abf9b0a2ac4cb
melonMochiRoll <soulhwasin@gmail.com>
2025-07-03 17:04:46 +0900

    :recycle: CSRF_TOKEN_HEADER_NAME, 소문자로 수정

--- src/common/constant/auth.constants.ts
+++ src/common/constant/auth.constants.ts
@@ -4,4 +4,4 @@ export const ACCESS_TOKEN_COOKIE_NAME='accessToken';
 export const REFRESH_TOKEN_COOKIE_NAME='refreshToken';
 
 export const CSRF_TOKEN_COOKIE_NAME='csrfToken';
-export const CSRF_TOKEN_HEADER_NAME='X-CSRF-TOKEN';
+export const CSRF_TOKEN_HEADER_NAME='x-csrf-token';
