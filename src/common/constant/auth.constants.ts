b470762615abcbe62ff6f29d52c91c065fa7626f
melonMochiRoll <soulhwasin@gmail.com>
2025-07-07 20:38:20 +0900

    :sparkles: OAUTH2_CSRF_STATE_COOKIE_NAME 정의

--- src/common/constant/auth.constants.ts
+++ src/common/constant/auth.constants.ts
@@ -1,7 +1,9 @@
 
-export const ACCESS_TOKEN_COOKIE_NAME='accessToken';
+export const ACCESS_TOKEN_COOKIE_NAME = 'accessToken';
 
-export const REFRESH_TOKEN_COOKIE_NAME='refreshToken';
+export const REFRESH_TOKEN_COOKIE_NAME = 'refreshToken';
 
-export const CSRF_TOKEN_COOKIE_NAME='csrfToken';
-export const CSRF_TOKEN_HEADER_NAME='x-csrf-token';
+export const CSRF_TOKEN_COOKIE_NAME = 'csrfToken';
+export const CSRF_TOKEN_HEADER_NAME = 'x-csrf-token';
+
+export const OAUTH2_CSRF_STATE_COOKIE_NAME = 'oauth2CsrfToken';
