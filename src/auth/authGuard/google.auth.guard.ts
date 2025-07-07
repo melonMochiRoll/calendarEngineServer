ecf22e5715689b00ca1757afaeb42ea7eefad490
melonMochiRoll <soulhwasin@gmail.com>
2025-07-07 20:27:56 +0900

    :recycle: 개발, 배포 환경에 맞춰 적절한 origin을 제공하도록 수정

--- src/auth/authGuard/google.auth.guard.ts
+++ src/auth/authGuard/google.auth.guard.ts
@@ -2,6 +2,7 @@ import { ExecutionContext, Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { INTERNAL_SERVER_MESSAGE } from "src/common/constant/error.message";
 import { RedirectingException } from "src/common/exception/redirecting.exception";
+import { getOrigin } from "src/common/function/getOrigin";
 import { Users } from "src/entities/Users";
 
 @Injectable()
@@ -13,7 +14,7 @@ export class GoogleAuthGuard extends AuthGuard('google') {
   handleRequest<TUser = Users>(err: Error | null, user: TUser | false, info: never, ctx: ExecutionContext) {
     if (err || !user) {
       console.error(err);
-      throw new RedirectingException(`${process.env.SERVER_ORIGIN}/login?error=${INTERNAL_SERVER_MESSAGE}`);
+      throw new RedirectingException(`${getOrigin()}/login?error=${INTERNAL_SERVER_MESSAGE}`);
     }
 
     return user;
