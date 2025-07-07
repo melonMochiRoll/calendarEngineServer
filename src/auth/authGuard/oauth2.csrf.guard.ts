e4c7d33f44b7bdab3123f6e56d252aa9e3ab9557
melonMochiRoll <soulhwasin@gmail.com>
2025-07-07 20:32:32 +0900

    :recycle: CSRF 변조 방지 로직 개선

--- src/auth/authGuard/oauth2.csrf.guard.ts
+++ src/auth/authGuard/oauth2.csrf.guard.ts
@@ -1,22 +1,27 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { CacheManagerService } from "src/cacheManager/cacheManager.service";
+import { OAUTH2_CSRF_STATE_COOKIE_NAME } from "src/common/constant/auth.constants";
 import { INTERNAL_SERVER_MESSAGE } from "src/common/constant/error.message";
 import { RedirectingException } from "src/common/exception/redirecting.exception";
+import { getOrigin } from "src/common/function/getOrigin";
 
 @Injectable()
 export class OAuth2CSRFGuard implements CanActivate {
-  constructor(
-    private cacheManagerService: CacheManagerService,
-  ) {}
-
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
 
-    const state = await this.cacheManagerService.getGuestCache(request.query?.state);
+    const queryState = request.query?.state;
+    const oauth2CsrfToken = request.cookies[OAUTH2_CSRF_STATE_COOKIE_NAME];
 
-    if (!state) {
-      throw new RedirectingException(`${process.env.SERVER_ORIGIN}/login?error=${INTERNAL_SERVER_MESSAGE}`);
+    if (!queryState || !oauth2CsrfToken || queryState !== oauth2CsrfToken) {
+      throw new RedirectingException(`${getOrigin()}/login?error=${INTERNAL_SERVER_MESSAGE}`);
     }
+
+    response.clearCookie(OAUTH2_CSRF_STATE_COOKIE_NAME, {
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production',
+    });
 
     return true;
   }
