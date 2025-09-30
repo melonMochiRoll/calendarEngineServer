de892bbc42a6818fac7a622d0d9e7a3650f03c6b
melonMochiRoll <soulhwasin@gmail.com>
2025-09-30 20:26:31 +0900

    :sparkles: getRoleMap 정의

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,17 +1,52 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import { ConflictException, Inject, Injectable } from "@nestjs/common";
 import handleError from "src/common/function/handleError";
 import { CreateRoleDTO } from "./dto/create.role.dto";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Roles } from "src/entities/Roles";
 import { Repository } from "typeorm";
 import { CONFLICT_MESSAGE } from "src/common/constant/error.message";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Cache } from 'cache-manager';
+import { ROLE_ID_MAP_KEY } from "src/common/constant/auth.constants";
 
 @Injectable()
 export class RolesService {
   constructor(
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
     @InjectRepository(Roles)
     private rolesRepository: Repository<Roles>,
   ) {}
+
+  async getRoleMap() {
+    const roleIdMap = await this.cacheManager.get(ROLE_ID_MAP_KEY);
+
+    if (roleIdMap) {
+      return roleIdMap;
+    }
+
+    try {
+      const roles = await this.rolesRepository.find({
+        select: {
+          id: true,
+          name: true,
+        },
+      });
+
+      const role_map = roles.reduce((map, role) => {
+        map[role.name] = role.id;
+        return map;
+      }, {});
+
+      if (roles) {
+        await this.cacheManager.set(ROLE_ID_MAP_KEY, role_map, 0);
+      }
+
+      return role_map;
+    } catch (err) {
+      handleError(err);
+    }
+  }
   
   async createRole(dto: CreateRoleDTO) {
     try {
