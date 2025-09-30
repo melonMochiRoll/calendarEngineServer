44f9f4ea68b429da07671bc053029b636746fd81
melonMochiRoll <soulhwasin@gmail.com>
2025-09-30 20:25:58 +0900

    :sparkles: ROLE_ID_MAP_KEY 정의

--- src/common/constant/auth.constants.ts
+++ src/common/constant/auth.constants.ts
@@ -7,3 +7,5 @@ export const CSRF_TOKEN_COOKIE_NAME = 'csrfToken';
 export const CSRF_TOKEN_HEADER_NAME = 'x-csrf-token';
 
 export const OAUTH2_CSRF_STATE_COOKIE_NAME = 'oauth2CsrfToken';
+
+export const ROLE_ID_MAP_KEY = 'role_id_map';
