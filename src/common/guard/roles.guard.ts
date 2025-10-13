f10694300328b05017a8e4ab9bf99d1be7f4b7e3
melonMochiRoll <soulhwasin@gmail.com>
2025-10-13 17:31:05 +0900

    :necktie: userRole, getUserRole메서드로 가져오도록 수정

--- src/common/guard/roles.guard.ts
+++ src/common/guard/roles.guard.ts
@@ -1,10 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext, BadRequestException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Roles } from '../decorator/roles.decorator';
-import { SharedspaceMembers } from 'src/entities/SharedspaceMembers';
 import { BAD_REQUEST_MESSAGE } from '../constant/error.message';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { SharedspacesService } from 'src/sharedspaces/sharedspaces.service';
 import { RolesService } from 'src/roles/roles.service';
 
@@ -14,8 +11,6 @@ export class RolesGuard implements CanActivate {
     private reflector: Reflector,
     private sharedspacesService: SharedspacesService,
     private rolesService: RolesService,
-    @InjectRepository(SharedspaceMembers)
-    private sharedspaceMembersRepository: Repository<SharedspaceMembers>,
   ) {}
   
   async canActivate(context: ExecutionContext) {
@@ -34,22 +29,13 @@ export class RolesGuard implements CanActivate {
       throw new BadRequestException(BAD_REQUEST_MESSAGE);
     }
 
-    const space = await this.sharedspacesService.getSharedspaceByUrl(user.id, url);
+    const space = await this.sharedspacesService.getSharedspaceByUrl(url);
 
     if (!space) {
       throw new BadRequestException(BAD_REQUEST_MESSAGE);
     }
 
-    const userRole = await this.sharedspaceMembersRepository.findOne({
-      select: {
-        SharedspaceId: true,
-        RoleId: true,
-      },
-      where: {
-        UserId: user?.id,
-        SharedspaceId: space?.id,
-      },
-    });
+    const userRole = await this.rolesService.getUserRole(user.id, space.id);
 
     const roleIdMap = await this.rolesService.getRoleMap();
     const roleIds = roles.map((role) => roleIdMap[role]);
