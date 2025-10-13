3bb3e2c4787fdcef3b3b971b195951726ceb80e9
melonMochiRoll <soulhwasin@gmail.com>
2025-10-13 16:41:42 +0900

    :sparkles: PublicAuthGuard 정의

--- src/auth/authGuard/jwt.auth.guard.ts
+++ src/auth/authGuard/jwt.auth.guard.ts
@@ -2,6 +2,7 @@ import { ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common
 import { AuthGuard } from "@nestjs/passport";
 import { ACCESS_DENIED_MESSAGE } from "src/common/constant/error.message";
 import { ACCESS_TOKEN_COOKIE_NAME, REFRESH_TOKEN_COOKIE_NAME } from "src/common/constant/auth.constants";
+import { Users } from "src/entities/Users";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -10,6 +11,19 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     request.res = context.switchToHttp().getResponse();
 
     return await super.canActivate(context) as boolean;
+  }
+}
+
+export class PublicAuthGuard extends AuthGuard('jwt') {
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    request.res = context.switchToHttp().getResponse();
+
+    return await super.canActivate(context) as boolean;
+  }
+
+  handleRequest<TUser = Users>(err: any, user: TUser | null) {
+    return user;
   }
 }
 
