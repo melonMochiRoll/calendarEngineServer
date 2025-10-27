027ce56e93371884852cfa28dec556bcc33fe46b
melonMochiRoll <soulhwasin@gmail.com>
2025-10-27 18:31:56 +0900

    :fire: PubilcSpaceGuard 제거

--- src/sharedspaces/sharedspaces.module.ts
+++ src/sharedspaces/sharedspaces.module.ts
@@ -5,7 +5,6 @@ import { SharedspacesController } from './sharedspaces.controller';
 import { SharedspacesService } from './sharedspaces.service';
 import { IsExistSpaceConstraint } from 'src/common/validator/IsExistSpace';
 import { SharedspaceMembers } from 'src/entities/SharedspaceMembers';
-import { PublicSpaceGuard } from 'src/common/guard/public.space.guard';
 import { Roles } from 'src/entities/Roles';
 import { Chats } from 'src/entities/Chats';
 import { EventsModule } from 'src/events/events.module';
@@ -36,7 +35,6 @@ import { UsersModule } from 'src/users/users.module';
   providers: [
     SharedspacesService,
     IsExistSpaceConstraint,
-    PublicSpaceGuard,
   ],
   exports: [
     SharedspacesService,
