d1a215db58182ae180d5d0d2f2207fda8efede70
melonMochiRoll <soulhwasin@gmail.com>
2025-10-29 18:41:58 +0900

    :sparkles: CONFLICT_OWNER_MESSAGE 정의

--- src/common/constant/error.message.ts
+++ src/common/constant/error.message.ts
@@ -1,24 +1,26 @@
 
-export const BAD_REQUEST_MESSAGE = '요청이 잘못되었습니다. 입력한 내용이 올바른 형식인지 확인해 주세요.'; // status: 400
+export const BAD_REQUEST_MESSAGE = '요청이 잘못되었습니다. 입력한 내용이 올바른 형식인지 확인해 주세요.';
 
-export const UNAUTHORIZED_MESSAGE = '로그인이 필요합니다.'; // status: 401
+export const UNAUTHORIZED_MESSAGE = '로그인이 필요합니다.';
 
 export const INCORRECT_CREDENTIALS_MESSAGE = '이메일 혹은 비밀번호를 확인해주세요';
 
-export const ACCESS_DENIED_MESSAGE = '접근 권한이 없습니다.'; // status: 403
+export const ACCESS_DENIED_MESSAGE = '접근 권한이 없습니다.';
 
-export const MISMATCH_STATE_MESSAGE = 'state 값이 다릅니다.'; // status: 403
+export const MISMATCH_STATE_MESSAGE = 'state 값이 다릅니다.';
 
-export const TOKEN_EXPIRED = '토큰이 만료되었거나 유효하지 않습니다. 다시 로그인해주세요.'; // status: 403
+export const TOKEN_EXPIRED = '토큰이 만료되었거나 유효하지 않습니다. 다시 로그인해주세요.';
 
-export const NOT_FOUND_SPACE_MESSAGE = '존재하지 않는 스페이스입니다.'; // status: 404
+export const NOT_FOUND_SPACE_MESSAGE = '존재하지 않는 스페이스입니다.';
 
-export const NOT_FOUND_RESOURCE = '요청하신 리소스를 찾을 수 없습니다.'; // status: 404
+export const NOT_FOUND_RESOURCE = '요청하신 리소스를 찾을 수 없습니다.';
 
-export const CONFLICT_MESSAGE = '요청한 데이터와 서버의 데이터 상태가 일치하지 않습니다. 최신 상태를 확인하세요.'; // status: 409;
+export const CONFLICT_MESSAGE = '요청한 데이터와 서버의 데이터 상태가 일치하지 않습니다. 최신 상태를 확인하세요.';
 
-export const CONFLICT_ACCOUNT_MESSAGE = '이미 가입된 이메일입니다.'; // status: 409;
+export const CONFLICT_ACCOUNT_MESSAGE = '이미 가입된 이메일입니다.';
 
-export const CONFLICT_REQUEST_MESSAGE = '이미 요청을 보냈습니다.'; // status: 409;
+export const CONFLICT_REQUEST_MESSAGE = '이미 요청을 보냈습니다.';
 
-export const INTERNAL_SERVER_MESSAGE = '서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.'; // status: 500
+export const CONFLICT_OWNER_MESSAGE = '동일한 유저로 바꿀수 없습니다.';
+
+export const INTERNAL_SERVER_MESSAGE = '서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.';
