89137f91ac522504f3bb14e678d8aac423c19d56
melonMochiRoll <soulhwasin@gmail.com>
2025-11-04 18:25:19 +0900

    :fire: OwnerId 프로퍼티 삭제

--- src/sharedspaces/dto/update.sharedspace.owner.dto.ts
+++ src/sharedspaces/dto/update.sharedspace.owner.dto.ts
@@ -2,13 +2,6 @@ import { Type } from "class-transformer";
 import { IsExistUser } from "src/common/validator/IsExistUser";
 
 export class UpdateSharedspaceOwnerDTO {
-
-  @IsExistUser({
-    message: '$property은 존재하지 않는 회원입니다.'
-  })
-  @Type(() => Number)
-  OwnerId: number;
-
   @IsExistUser({
     message: '$property은 존재하지 않는 회원입니다.'
   })
