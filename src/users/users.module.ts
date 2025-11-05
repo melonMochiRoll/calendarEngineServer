37c50115f478ee35094b64fee9dcb14e9f41f8ed
melonMochiRoll <soulhwasin@gmail.com>
2025-11-05 18:17:54 +0900

    :heavy_plus_sign: SharedspacesModule, SharedspaceMembers 의존성 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,13 +4,17 @@ import { UsersService } from './users.service';
 import { Users } from 'src/entities/Users';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RefreshTokens } from 'src/entities/RefreshTokens';
+import { SharedspaceMembers } from 'src/entities/SharedspaceMembers';
+import { SharedspacesModule } from 'src/sharedspaces/sharedspaces.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Users,
       RefreshTokens,
+      SharedspaceMembers,
     ]),
+    SharedspacesModule,
   ],
   controllers: [ UsersController ],
   providers: [
