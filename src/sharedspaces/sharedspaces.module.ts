ea88d12a3ae631e0b435b59a99988321ddd02779
melonMochiRoll <soulhwasin@gmail.com>
2025-11-05 16:48:11 +0900

    :fire: 불필요한 의존성 삭제

--- src/sharedspaces/sharedspaces.module.ts
+++ src/sharedspaces/sharedspaces.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Sharedspaces } from 'src/entities/Sharedspaces';
 import { SharedspacesController } from './sharedspaces.controller';
 import { SharedspacesService } from './sharedspaces.service';
-import { IsExistSpaceConstraint } from 'src/common/validator/IsExistSpace';
 import { SharedspaceMembers } from 'src/entities/SharedspaceMembers';
 import { Roles } from 'src/entities/Roles';
 import { Chats } from 'src/entities/Chats';
@@ -13,7 +12,6 @@ import { AwsModule } from 'src/aws/aws.module';
 import { RefreshTokens } from 'src/entities/RefreshTokens';
 import { Users } from 'src/entities/Users';
 import { RolesModule } from 'src/roles/roles.module';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
@@ -29,12 +27,10 @@ import { UsersModule } from 'src/users/users.module';
     EventsModule,
     AwsModule,
     RolesModule,
-    UsersModule,
   ],
   controllers: [ SharedspacesController ],
   providers: [
     SharedspacesService,
-    IsExistSpaceConstraint,
   ],
   exports: [
     SharedspacesService,
