2b729aaa2d399c8ce20791ceaa6d8cc921f8b0d2
melonMochiRoll <soulhwasin@gmail.com>
2025-11-05 19:16:21 +0900

    :sparkles: SharedspacesUsersContoller 정의

---
+++ src/users/sharedspaces.users.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Param, ParseIntPipe, Query, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/authGuard/jwt.auth.guard";
+import { UsersService } from "./users.service";
+import { User } from "src/common/decorator/user.decorator";
+import { Users } from "src/entities/Users";
+
+@Controller('api/sharedspaces')
+export class SharedspacesUsersContoller {
+  constructor(
+    private usersService: UsersService,
+  ) {}
+  
+  @UseGuards(JwtAuthGuard)
+  @Get(':url/users/search')
+  searchUsers(
+    @Param('url') url: string,
+    @Query('query') query: string,
+    @Query('page', ParseIntPipe) page: number,
+    @User() user: Users,
+  ) {
+    return this.usersService.searchUsers(url, query, page, user.id);
+  }
+}
