4dfdebb641603688c7c5b088b12e665c5bc57767
melonMochiRoll <soulhwasin@gmail.com>
2025-11-05 16:49:03 +0900

    :fire: 이메일 검증 로직은 비즈니스 로직내에 존재하기때문에 삭제

--- src/users/dto/create.user.dto.ts
+++ src/users/dto/create.user.dto.ts
@@ -1,11 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
-import { IsNotExistEmail } from "src/common/validator/IsNotExistEmail";
 
 export class CreateUserDTO {
-
-  @IsNotExistEmail({
-    message: '이미 존재하는 $property입니다.',
-  })
   @IsEmail()
   email: string;
 
