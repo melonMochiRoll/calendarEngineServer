59e21448832df87b48a47a9a64dc9e45134df4a7
melonMochiRoll <soulhwasin@gmail.com>
2025-12-01 19:06:51 +0900

    :sparkles: DATA_NOT_FOUND 정의

--- src/common/constant/auth.constants.ts
+++ src/common/constant/auth.constants.ts
@@ -9,3 +9,5 @@ export const CSRF_TOKEN_HEADER_NAME = 'x-csrf-token';
 export const OAUTH2_CSRF_STATE_COOKIE_NAME = 'oauth2CsrfToken';
 
 export const ROLES_ARRAY_KEY = 'roles_array';
+
+export const DATA_NOT_FOUND = 'data_not_found';
