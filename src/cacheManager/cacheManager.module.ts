53399159c1a47b451e5bbdb160c7591563122e10
melonMochiRoll <soulhwasin@gmail.com>
2025-12-03 19:00:40 +0900

    :art: redis 통합

--- src/cacheManager/cacheManager.module.ts
+++ src/cacheManager/cacheManager.module.ts
@@ -2,14 +2,23 @@ import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
 import { CacheManagerService } from './cacheManager.service';
 import { OAuth2CSRFGuard } from 'src/auth/authGuard/oauth2.csrf.guard';
+import KeyvRedis from '@keyv/redis';
+import 'dotenv/config';
 
 @Module({
   imports: [
-    CacheModule.register({
+    CacheModule.registerAsync({
       isGlobal: true,
-      max: 100,
-      ttl: 5000,
-    })
+      useFactory: async () => {
+        const url = `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}`;
+        return {
+          stores: [
+            new KeyvRedis(url),
+          ],
+          ttl: 5000,
+        };
+      },
+    }),
   ],
   providers: [
     CacheManagerService,
