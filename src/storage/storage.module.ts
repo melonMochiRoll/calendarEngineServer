2a07e6fe76b6ee62f8f1d2a706855116d4e0d934
melonMochiRoll <soulhwasin@gmail.com>
2026-01-21 13:18:27 +0900

    :sparkles: StorageR2Service 정의

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -2,17 +2,33 @@ import { Module } from '@nestjs/common';
 import { STORAGE_SERVICE } from 'src/typings/types';
 import { StorageOciService } from './storage.oci.service';
 import { StorageS3Service } from './storage.s3.service';
+import { StorageR2Service } from './storage.r2.service';
 
 @Module({
   providers: [
     StorageS3Service,
     StorageOciService,
+    StorageR2Service,
     {
       provide: STORAGE_SERVICE,
-      useFactory: (s3: StorageS3Service, oci: StorageOciService) => {
-        return process.env.STORAGE_PROVIDER === 's3' ? s3 : oci;
+      useFactory: (
+        s3: StorageS3Service,
+        oci: StorageOciService,
+        r2: StorageR2Service
+      ) => {
+        const storageMap = {
+          's3': s3,
+          'oci': oci,
+          'r2': r2,
+        };
+        
+        return storageMap[process.env.STORAGE_PROVIDER];
       },
-      inject: [ StorageS3Service, StorageOciService ],
+      inject: [
+        StorageS3Service,
+        StorageOciService,
+        StorageR2Service,
+      ],
     },
   ],
   exports: [ STORAGE_SERVICE ],
---
+++ src/storage/storage.r2.service.ts
@@ -0,0 +1,94 @@
+import { DeleteObjectCommand, GetObjectCommand, PutObjectCommand, S3Client, waitUntilObjectNotExists } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { Injectable } from "@nestjs/common";
+import handleError from "src/common/function/handleError";
+import { IStorageService } from "src/typings/types";
+
+@Injectable()
+export class StorageR2Service implements IStorageService {
+  r2Client: S3Client;
+  
+  constructor() {
+    this.r2Client = new S3Client({
+      region: process.env.R2_REGION,
+      endpoint: process.env.R2_ENDPOINT,
+      credentials: {
+        accessKeyId: process.env.R2_ACCESS_KEY_ID,
+        secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+      },
+    });
+  }  
+  
+  async uploadFile(
+    file: Express.Multer.File,
+    key: string
+  ) {
+    const command = new PutObjectCommand({
+      Bucket: process.env.R2_BUCKET_NAME,
+      Key: key,
+      Body: file.buffer,
+      CacheControl: 'public, max-age=31536000, immutable',
+    });
+
+    try {
+      await this.r2Client.send(command);
+    } catch (err) {
+      handleError(err);
+    }
+
+    return true;
+  }
+  async deleteFile(key: string) {
+    const command = new DeleteObjectCommand({
+      Bucket: process.env.R2_BUCKET_NAME,
+      Key: key,
+    });
+
+    try {
+      await this.r2Client.send(command);
+
+      await waitUntilObjectNotExists(
+        { 
+          client: this.r2Client,
+          maxWaitTime: 6,
+          minDelay: 5,
+        },
+        {
+          Bucket: process.env.R2_BUCKET_NAME,
+          Key: key,
+        },
+      );
+    } catch (err) {
+      handleError(err);
+    }
+
+    return true;
+  }
+  async generatePresignedGetUrl(key: string) {
+    try {
+      const command = new GetObjectCommand({
+        Bucket: process.env.R2_BUCKET_NAME,
+        Key: key,
+      });
+
+      return await getSignedUrl(this.r2Client, command, {
+        expiresIn: 3600 * 3,
+      });
+    } catch (err) {
+      handleError(err);
+    }
+  }
+  async generatePresignedPutUrl(key: string) {
+    try {
+      const command = new PutObjectCommand({
+        Bucket: process.env.R2_BUCKET_NAME,
+        Key: key,
+        CacheControl: 'public, max-age=31536000, immutable',
+      });
+
+      return await getSignedUrl(this.r2Client, command);
+    } catch (err) {
+      handleError(err);
+    }
+  }
+}
