65312be3366dce965ea28b77b76abaf1e1d46cdd
melonMochiRoll <soulhwasin@gmail.com>
2026-02-04 15:42:00 +0900

    :sparkles: getInvites, 초대 목록 조회 구현

--- src/invites/invites.controller.ts
+++ src/invites/invites.controller.ts
@@ -1,9 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, ParseIntPipe, Query, UseGuards } from "@nestjs/common";
 import { InvitesService } from "./invites.service";
+import { JwtAuthGuard } from "src/auth/authGuard/jwt.auth.guard";
+import { Users } from "src/entities/Users";
+import { User } from "src/common/decorator/user.decorator";
 
 @Controller('api/invites')
 export class InvitesContoller {
   constructor(
     private invitesService: InvitesService
   ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  getInvites(
+    @Query('page', ParseIntPipe) page: number,
+    @User() user: Users,
+  ) {
+    return this.invitesService.getInvites(user.id);
+  }
 }
--- src/invites/invites.service.ts
+++ src/invites/invites.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import handleError from "src/common/function/handleError";
 import { Invites } from "src/entities/Invites";
-import { Repository } from "typeorm";
+import { MoreThan, Repository } from "typeorm";
+import dayjs from "dayjs";
+import { INVITE_STATUS } from "src/common/constant/constants";
 
 @Injectable()
 export class InvitesService {
@@ -9,4 +12,40 @@ export class InvitesService {
     @InjectRepository(Invites)
     private invitesRepository: Repository<Invites>,
   ) {}
+
+  async getInvites(
+    UserId: number,
+    page = 1,
+    limit = 7,
+  ) {
+    try {
+      const invites = await this.invitesRepository.find({
+        select: {
+          id: true,
+          createdAt: true,
+          Sharedspace: {
+            name: true,
+            url: true,
+          },
+        },
+        relations: {
+          Sharedspace: true,
+        },
+        where: {
+          InviteeId: UserId,
+          status: INVITE_STATUS.PENDING,
+          expiredAt: MoreThan(dayjs().toDate()),
+        },
+        order: {
+          createdAt: 'DESC',
+        },
+        skip: (page - 1) * limit,
+        take: limit,
+      });
+
+      return invites;
+    } catch (err) {
+      handleError(err);
+    }
+  }
 }
