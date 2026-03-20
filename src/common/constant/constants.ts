8c3a7aace0d5938149ab80597a694a0d0400d6ba
melonMochiRoll <soulhwasin@gmail.com>
2026-03-20 16:05:59 +0900

    :recycle: IMAGE_STATUS, DELETED 프로퍼티 추가

--- src/common/constant/constants.ts
+++ src/common/constant/constants.ts
@@ -4,6 +4,7 @@ export const NANOID_SHAREDSPACE_URL_LENGTH = 9;
 export const IMAGE_STATUS = {
   PENDING: 'pending',
   ACTIVE: 'active',
+  DELETED: 'deleted',
 };
 
 export const INVITE_STATUS = {
