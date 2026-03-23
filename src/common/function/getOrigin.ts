4dfacd45c13b18a7ac1bceba9ffa850e60d90bfc
melonMochiRoll <soulhwasin@gmail.com>
2026-03-23 17:22:58 +0900

    :recycle: 환경 변수명 수정

--- src/common/function/getOrigin.ts
+++ src/common/function/getOrigin.ts
@@ -1,4 +1,4 @@
 
 export function getOrigin() {
-  return process.env.NODE_ENV === 'development' ? process.env.DEV_FRONT_SERVER_ORIGIN : process.env.SERVER_ORIGIN;
+  return process.env.NODE_ENV === 'development' ? process.env.FRONT_SERVER_ORIGIN : process.env.SERVER_ORIGIN;
 }
