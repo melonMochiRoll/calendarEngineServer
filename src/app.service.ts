ea4e3ed8146e978156cff9cdcc845cc3ba320ae7
melonMochiRoll <soulhwasin@gmail.com>
2026-03-24 15:28:08 +0900

    :sparkles: 서버 구동시 initGhostUser 호출 하도록 수정

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,16 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { RolesService } from "./roles/roles.service";
+import { UsersService } from "./users/users.service";
 
 @Injectable()
 export class AppService implements OnModuleInit {
   constructor(
     private rolesService: RolesService,
+    private usersService: UsersService,
   ) {}
 
   async onModuleInit() {
+    await this.usersService.initGhostUser();
     await this.rolesService.initRoles();
     await this.rolesService.getRolesArray();
   }
