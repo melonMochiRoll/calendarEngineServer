9f0e39ee63402cd597310f636c306367ea04f87e
melonMochiRoll <soulhwasin@gmail.com>
2026-03-30 19:32:36 +0900

    :sparkles: todos, Cache-Aside 적용 및 수정시 캐싱 무효화 적용

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, ForbiddenException, Injectable } from "@nestjs/common";
+import { BadRequestException, ForbiddenException, Inject, Injectable } from "@nestjs/common";
 import dayjs from "dayjs";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Between, Like, Repository } from "typeorm";
@@ -8,10 +8,15 @@ import { UpdateTodoDto } from "./dto/update.todo.dto";
 import { ACCESS_DENIED_MESSAGE, BAD_REQUEST_MESSAGE } from "src/common/constant/error.message";
 import { SharedspacesService } from "src/sharedspaces/sharedspaces.service";
 import { RolesService } from "src/roles/roles.service";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Cache } from 'cache-manager';
+import { CACHE_EMPTY_SYMBOL } from "src/common/constant/constants";
 
 @Injectable()
 export class TodosService {
   constructor(
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
     @InjectRepository(Todos)
     private todosRepository: Repository<Todos>,
     private sharedspacesService: SharedspacesService,
@@ -38,6 +43,26 @@ export class TodosService {
           metaData: { spaceUrl: space.url },
         });
       }
+    }
+
+    const cacheKey = `todos:${space.url}:${year}-${month}`;
+
+    const cachedItem = await this.cacheManager.get<Todos[] | typeof CACHE_EMPTY_SYMBOL>(cacheKey);
+
+    if (cachedItem) {
+      if (cachedItem === CACHE_EMPTY_SYMBOL) {
+        return null;
+      }
+
+      const cachedTodosMap = cachedItem.reduce((map, todo) => {
+        if (!map[String(todo.date)]) {
+          map[String(todo.date)] = [];
+        }
+        map[String(todo.date)].push(todo);
+        return map;
+      }, {});
+
+      return cachedTodosMap;
     }
 
     const todos = await this.todosRepository.find({
@@ -69,6 +94,15 @@ export class TodosService {
         endTime: 'ASC',
       },
     });
+
+    const minute = 60000;
+
+    if (!todos) {
+      await this.cacheManager.set(cacheKey, CACHE_EMPTY_SYMBOL, 1 * minute);
+      return null;
+    }
+
+    await this.cacheManager.set(cacheKey, todos, 1 * minute);
 
     const todosMap = todos.reduce((map, todo) => {
       if (!map[String(todo.date)]) {
@@ -165,11 +199,13 @@ export class TodosService {
       });
     }
 
-    await this.todosRepository.save({
+    const targetTodo = await this.todosRepository.save({
       ...dto,
       AuthorId: UserId,
       SharedspaceId: space.id,
     });
+
+    await this.invalidateTodosCache(space.url, targetTodo.date);
   }
 
   async updateTodo(
@@ -200,6 +236,8 @@ export class TodosService {
       ...rest,
       EditorId: UserId,
     });
+
+    await this.invalidateTodosCache(space.url, targetTodo.date);
   }
 
   async deleteTodo(
@@ -225,6 +263,8 @@ export class TodosService {
     }
 
     await this.todosRepository.delete(todoId);
+
+    await this.invalidateTodosCache(space.url, targetTodo.date);
   }
 
   async searchTodos(
@@ -277,5 +317,12 @@ export class TodosService {
       items: todoRecords,
       hasMoreData: !Boolean(page * limit >= totalCount),
     };
+  }
+
+  async invalidateTodosCache(url: string, date: Date) {
+    const targetDate = dayjs(date);
+    const [ year, month ] = [ targetDate.year(), `${targetDate.month() + 1}`.padStart(2, '0') ];
+
+    await this.cacheManager.del(`todos:${url}:${year}-${month}`);
   }
 }
