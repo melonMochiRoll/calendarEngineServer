b75f26d4ac1b6548d19d4a6b0c28da6146b6bc76
melonMochiRoll <soulhwasin@gmail.com>
2026-04-02 14:58:35 +0900

    :heavy_plus_sign: TaskModule 의존성 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,7 @@ import { Roles } from 'src/entities/Roles';
 import { ChatsModule } from './chats/chats.module';
 import { InvitesModule } from './invites/invites.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
@@ -49,6 +50,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     StorageModule,
     ChatsModule,
     InvitesModule,
+    TaskModule,
   ],
   controllers: [],
   providers: [
