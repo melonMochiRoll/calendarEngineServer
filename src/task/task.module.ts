d84bdc525ce65329b09e116650d1753b2f0e8d8d
melonMochiRoll <soulhwasin@gmail.com>
2026-04-14 18:37:30 +0900

    :heavy_plus_sign: BatchScheduler, repository 패턴을 위한 의존성 추가

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,11 +3,13 @@ import { TaskService } from './task.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { UsersModule } from 'src/users/users.module';
+import { BatchScheduler } from 'src/entities/BatchScheduler';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Users,
+      BatchScheduler,
     ]),
     UsersModule,
   ],
