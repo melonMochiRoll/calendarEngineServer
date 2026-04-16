cf2bed65a132128c6ce3989dedce90921bf4f9d3
melonMochiRoll <soulhwasin@gmail.com>
2026-04-16 15:46:32 +0900

    :fire: 역할은 서비스 내에서 검증하기로 수정됐으므로 역할 데코레이터 제거

--- src/common/decorator/owner.only.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { applyDecorators } from "@nestjs/common";
-import { Roles } from 'src/common/decorator/roles.decorator';
-import { SharedspaceMembersRoles } from "src/typings/types";
-
-export function OwnerOnlyRoles() {
-  return applyDecorators(
-    Roles([SharedspaceMembersRoles.OWNER])
-  );
-}
