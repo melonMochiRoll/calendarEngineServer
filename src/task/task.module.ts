33047a887ed1ab0f913c70e3f19cd1bf9b4fb42f
melonMochiRoll <soulhwasin@gmail.com>
2026-04-18 15:30:32 +0900

    :heavy_plus_sign: StorageModule, Images 의존성 추가

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -4,14 +4,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
 import { BatchScheduler } from 'src/entities/BatchScheduler';
 import { SharedspacesModule } from 'src/sharedspaces/sharedspaces.module';
+import { Images } from 'src/entities/Images';
+import { StorageModule } from 'src/storage/storage.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       BatchScheduler,
+      Images,
     ]),
     UsersModule,
     SharedspacesModule,
+    StorageModule,
   ],
   providers: [ TaskService ],
 })
