6d87b1fd043b45234c26fef869545e02e58f18f4
melonMochiRoll <soulhwasin@gmail.com>
2026-04-23 18:47:23 +0900

    :sparkles: UUIDV7Transformer 정의

---
+++ src/common/function/uuidv7Transformer.ts
@@ -0,0 +1,21 @@
+import { ValueTransformer } from "typeorm";
+
+export class UUIDV7Transformer implements ValueTransformer {
+
+  to(value: string) {
+    if (!value) {
+      return;
+    }
+
+    return Buffer.from(value.replace(/-/g, ''), 'hex');
+  }
+
+  from(value: Buffer) {
+    if (!value) {
+      return;
+    }
+
+    const hex = value.toString('hex');
+    return `${hex.slice(0, 8)}-${hex.slice(8, 12)}-${hex.slice(12, 16)}-${hex.slice(16, 20)}-${hex.slice(20)}`;
+  }
+}
