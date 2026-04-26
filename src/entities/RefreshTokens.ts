a5897a98a78ee247931b33c3379c0f224bdbd3e9
melonMochiRoll <soulhwasin@gmail.com>
2026-04-26 16:15:04 +0900

    :card_file_box: jti 타입을 char로 변경하고 적절한 최대 길이를 설정하여 최적화

--- src/entities/RefreshTokens.ts
+++ src/entities/RefreshTokens.ts
@@ -1,13 +1,14 @@
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Users } from "./Users";
 import { UUIDV7Transformer } from "src/common/function/uuidv7Transformer";
+import { REFRESH_TOKEN_JTI_LENGTH } from "src/common/constant/constants";
 
 @Entity({ name: 'refreshTokens' })
 export class RefreshTokens {
   @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
   id: number;
 
-  @Column({ type: 'varchar', unique: true })
+  @Column({ type: 'char', length: REFRESH_TOKEN_JTI_LENGTH, unique: true })
   jti: string;
 
   @Column({ type: 'binary', name: 'UserId', length: 16, transformer: new UUIDV7Transformer() })
