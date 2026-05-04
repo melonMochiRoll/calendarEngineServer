93c79f2e2c226022195c559d9aa19e5369c9cf07
melonMochiRoll <soulhwasin@gmail.com>
2026-05-04 17:51:17 +0900

    :recycle: 더이상 불필요한 메서드 호출 및 async 키워드 제거

--- src/auth/authGuard/jwt.auth.guard.ts
+++ src/auth/authGuard/jwt.auth.guard.ts
@@ -6,21 +6,15 @@ import { Users } from "src/entities/Users";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  async canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    request.res = context.switchToHttp().getResponse();
-
-    return await super.canActivate(context) as boolean;
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context) as boolean;
   }
 }
 
 @Injectable()
 export class PublicAuthGuard extends AuthGuard('jwt') {
-  async canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    request.res = context.switchToHttp().getResponse();
-
-    return await super.canActivate(context) as boolean;
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context) as boolean;
   }
 
   handleRequest<TUser = Users>(err: any, user: TUser | null) {
@@ -34,8 +28,8 @@ export class PublicAuthGuard extends AuthGuard('jwt') {
 
 @Injectable()
 export class IsNotJwtAuthenicatedGuard extends AuthGuard('jwt') {
-  async canActivate(context: ExecutionContext) {
-    return await super.canActivate(context) as boolean;
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context) as boolean;
   }
 
   handleRequest<TUser = Users>(err: any, user: TUser | null) {
