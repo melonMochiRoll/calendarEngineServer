a44887a128f7e15d475a14a5542101e84a715ab4
melonMochiRoll <soulhwasin@gmail.com>
2026-05-05 15:44:00 +0900

    :heavy_plus_sign: ChatsModule, UsersModule, 의존성 주입

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,7 +1,13 @@
 import { Module } from "@nestjs/common";
 import { EventsGateway } from "./events.gateway";
+import { ChatsModule } from "src/chats/chats.module";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
+  imports: [
+    ChatsModule,
+    UsersModule,
+  ],
   providers: [ EventsGateway ],
   exports: [ EventsGateway ],
 })
