c35f6147dec0052e51a34316e1f1b5379026614b
melonMochiRoll <soulhwasin@gmail.com>
2026-05-29 16:25:19 +0900

    :sparkles: createChatSpace 엔드포인트 정의

--- src/chats/chats.controller.ts
+++ src/chats/chats.controller.ts
@@ -3,6 +3,9 @@ import { ChatsService } from "./chats.service";
 import { JwtAuthGuard } from "src/auth/authGuard/jwt.auth.guard";
 import { CSRFAuthGuard } from "src/auth/authGuard/csrf.auth.guard";
 import { GeneratePresignedPutUrlDTO } from "./dto/generate.presigned.put.url.dto";
+import { User } from "src/common/decorator/user.decorator";
+import { Users } from "src/entities/Users";
+import { CreatChatspaceDTO } from "./dto/create.chatspace.dto";
 
 @Controller('api')
 export class ChatsController {
@@ -17,5 +20,14 @@ export class ChatsController {
     @Body() dto: GeneratePresignedPutUrlDTO,
   ) {
     return this.chatsService.generatePresignedPutUrl(url, dto);
+  }
+
+  @UseGuards(JwtAuthGuard, CSRFAuthGuard)
+  @Post('chatspaces')
+  createChatSpace(
+    @User() user: Users,
+    @Body() dto: CreatChatspaceDTO,
+  ) {
+    return this.chatsService.createChatSpace(user.id, dto);
   }
 }
