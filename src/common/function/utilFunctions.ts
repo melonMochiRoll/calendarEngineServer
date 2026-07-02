f0d846b99eaa6d4fdd44a3bcf4f322e54d71bfda
melonMochiRoll <soulhwasin@gmail.com>
2026-07-02 17:45:50 +0900

    :sparkles: stringToUUID 정의

--- src/common/function/utilFunctions.ts
+++ src/common/function/utilFunctions.ts
@@ -33,3 +33,11 @@ export const uuidToString = (value: Buffer) => {
   const hex = value.toString('hex');
   return `${hex.slice(0, 8)}-${hex.slice(8, 12)}-${hex.slice(12, 16)}-${hex.slice(16, 20)}-${hex.slice(20)}`;
 };
+
+export const stringToUUID = (value: string) => {
+  if (!value) {
+    return;
+  }
+
+  return Buffer.from(value.replace(/-/g, ''), 'hex');
+};
