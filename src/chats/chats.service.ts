8ce88fcd38b738e27801613cb23564d15697bf2a
melonMochiRoll <soulhwasin@gmail.com>
2026-07-07 15:32:24 +0900

    :sparkles: getSharedspaceChats, 정렬 기준 createdAt에서 id로 변경 및 hasMoreData 로직 개선

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -88,9 +88,9 @@ export class ChatsService {
         removedAt: IsNull(),
       },
       order: {
-        createdAt: 'DESC',
+        id: 'DESC',
       },
-      take: limit,
+      take: limit + 1,
     });
 
     if (!chatRecords.length) {
@@ -98,6 +98,12 @@ export class ChatsService {
         chats: [],
         hasMoreData: false,
       };
+    }
+
+    const hasMoreData = chatRecords.length > limit;
+
+    if (hasMoreData) {
+      chatRecords.pop();
     }
 
     const images = await this.chatImagesRepository.find({
@@ -139,17 +145,9 @@ export class ChatsService {
       };
     });
 
-    const totalCount = await this.chatsRepository.count({
-      where: {
-        SpaceId: space.id,
-        id: LessThan(chats[chats.length-1].id),
-        removedAt: IsNull(),
-      },
-    });
-
     return {
       chats,
-      hasMoreData: Boolean(totalCount),
+      hasMoreData,
     };
   }
 
