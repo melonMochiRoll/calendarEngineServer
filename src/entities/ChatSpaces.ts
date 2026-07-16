0156b02e13a81cc246eb38c10d90fee4562f723e
melonMochiRoll <soulhwasin@gmail.com>
2026-07-16 14:53:35 +0900

    :card_file_box: name 컬럼 추가

--- src/entities/ChatSpaces.ts
+++ src/entities/ChatSpaces.ts
@@ -1,11 +1,14 @@
 import { UUIDV7Transformer } from "src/common/transformer/uuidv7Transformer";
-import { Entity, JoinColumn, OneToOne, PrimaryColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from "typeorm";
 import { Spaces } from "./Spaces";
 
 @Entity({ name: 'chatspaces' })
 export class ChatSpaces {
   @PrimaryColumn({ type: 'binary', name: 'id', length: 16, transformer: new UUIDV7Transformer() })
   id: string;
+
+  @Column({ type: 'varchar', name: 'name', length: 30, nullable: true })
+  name: string | null;
 
   @OneToOne(() => Spaces, spaces => spaces.Chatspace, {
     onDelete: 'CASCADE',
