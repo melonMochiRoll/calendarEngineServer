af84a104b7116269d1e2ce83d1cf568cdbdf9199
melonMochiRoll <soulhwasin@gmail.com>
2026-07-20 16:20:08 +0900

    :recycle: getSharedspaceByUrl, fetchSharedspaceAndWrite 메서드 분리 및 가독성 개선

--- src/sharedspaces/sharedspaces.fetcher.ts
+++ src/sharedspaces/sharedspaces.fetcher.ts
@@ -2,53 +2,21 @@ import { Inject, Injectable, NotFoundException } from "@nestjs/common";
 import { CACHE_MANAGER } from "@nestjs/cache-manager";
 import { Cache } from 'cache-manager';
 import { InjectRepository } from "@nestjs/typeorm";
-import { Spaces } from "src/entities/Spaces";
 import { IsNull, Repository } from "typeorm";
-import { CACHE_EMPTY_SYMBOL } from "src/common/constant/constants";
 import { CacheItem, TSharedspaceDefault } from "src/typings/types";
 import dayjs from "dayjs";
 import { NOT_FOUND_SPACE_MESSAGE } from "src/common/constant/error.message";
+import { Sharedspaces } from "src/entities/Sharedspaces";
 
 @Injectable()
 export class SharedspaceFetcher {
   constructor(
     @Inject(CACHE_MANAGER)
     private cacheManager: Cache,
-    @InjectRepository(Spaces)
-    private spacesRepository: Repository<Spaces>,
+    @InjectRepository(Sharedspaces)
+    private sharedspacesRepository: Repository<Sharedspaces>,
   ) {}
   private refreshLock = new Set<String>();
-
-  async getSpaceByUrl(url: string) {
-    const cacheKey = `space:${url}`;
-
-    const cachedItem = await this.cacheManager.get<{ id: string, url: string, type: string } | typeof CACHE_EMPTY_SYMBOL>(cacheKey);
-
-    if (cachedItem) {
-      return cachedItem === CACHE_EMPTY_SYMBOL ? null : cachedItem;
-    }
-
-    const space = await this.spacesRepository.findOne({
-      select: {
-        id: true,
-        url: true,
-        type: true,
-      },
-      where: {
-        url,
-      },
-    });
-
-    const minute = 60000;
-
-    if (!space) {
-      await this.cacheManager.set(cacheKey, CACHE_EMPTY_SYMBOL, 1 * minute);
-      return null;
-    }
-
-    await this.cacheManager.set(cacheKey, space, 10 * minute);
-    return space;
-  }
 
   async getSharedspaceByUrl(
     url: string,
@@ -57,54 +25,6 @@ export class SharedspaceFetcher {
     const cacheKey = `sharedspace:${url}`;
 
     const cachedItem = await this.cacheManager.get<CacheItem<TSharedspaceDefault>>(cacheKey);
-
-    const fetchSharedspaceAndWrite = async (cacheKey: string) => {
-      const start = dayjs();
-      const result = await this.spacesRepository.findOne({
-        select: {
-          id: true,
-          url: true,
-          createdAt: true,
-          Sharedspace: {
-            name: true,
-            private: true,
-            OwnerId: true,
-          },
-        },
-        where: {
-          url,
-          removedAt: IsNull(),
-        },
-        relations: {
-          Sharedspace: true,
-        },
-      });
-      const delta = dayjs().diff(start);
-
-      if (!result) {
-        throw new NotFoundException(NOT_FOUND_SPACE_MESSAGE);
-      }
-
-      const { Sharedspace, ...rest } = result;
-
-      const space: TSharedspaceDefault = {
-        ...rest,
-        name: Sharedspace.name,
-        private: Sharedspace.private,
-        OwnerId: Sharedspace.OwnerId,
-      };
-
-      const minute = 60000;
-      const ttl = 0.1 * minute;
-
-      await this.cacheManager.set(cacheKey, {
-        value: space,
-        duration: delta,
-        expireTime: dayjs().valueOf() + ttl,
-      }, ttl);
-
-      return space;
-    }
 
     if (cachedItem) {
       const random = Math.log(Math.random());
@@ -115,7 +35,7 @@ export class SharedspaceFetcher {
         this.refreshLock.add(cacheKey);
 
         try {
-          await fetchSharedspaceAndWrite(cacheKey);
+          await this.fetchSharedspaceAndWrite(cacheKey, url);
         } finally {
           this.refreshLock.delete(cacheKey);
         }
@@ -124,7 +44,41 @@ export class SharedspaceFetcher {
       return cachedItem.value;
     }
 
-    const space = await fetchSharedspaceAndWrite(cacheKey);
+    const space = await this.fetchSharedspaceAndWrite(cacheKey, url);
+    return space;
+  }
+
+  async fetchSharedspaceAndWrite(cacheKey: string, url: string) {
+    const start = dayjs();
+    const space = await this.sharedspacesRepository.findOne({
+      select: {
+        id: true,
+        url: true,
+        name: true,
+        private: true,
+        OwnerId: true,
+        createdAt: true,
+      },
+      where: {
+        url,
+        removedAt: IsNull(),
+      },
+    });
+    const delta = dayjs().diff(start);
+
+    if (!space) {
+      throw new NotFoundException(NOT_FOUND_SPACE_MESSAGE);
+    }
+
+    const minute = 60000;
+    const ttl = 0.1 * minute;
+
+    await this.cacheManager.set(cacheKey, {
+      value: space,
+      duration: delta,
+      expireTime: dayjs().valueOf() + ttl,
+    }, ttl);
+
     return space;
   }
 
