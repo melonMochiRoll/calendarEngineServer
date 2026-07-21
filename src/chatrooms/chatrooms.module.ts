a28b4012daf2db5180a4357647de1e3d1ed9043f
melonMochiRoll <soulhwasin@gmail.com>
2026-07-21 16:42:03 +0900

    :sparkles: ChatRoomsFetcher, exports 배열에 추가

--- src/chatrooms/chatrooms.module.ts
+++ src/chatrooms/chatrooms.module.ts
@@ -20,6 +20,9 @@ import { ChatRoomsFetcher } from "./chatrooms.fetcher";
     ChatRoomsService,
     ChatRoomsFetcher,
   ],
+  exports: [
+    ChatRoomsFetcher,
+  ],
 })
 
 export class ChatRoomsModule {}
