aa62b3d5f72a611426e572e99140aa27123b3203
melonMochiRoll <soulhwasin@gmail.com>
2026-08-05 14:53:37 +0900

    :recycle: 내부 변수 선언을 클래스 필드 문법으로 리팩토링

--- src/common/pipe/uuidv7.validation.pipe.ts
+++ src/common/pipe/uuidv7.validation.pipe.ts
@@ -3,12 +3,13 @@ import { BAD_REQUEST_MESSAGE } from "../constant/error.message";
 
 @Injectable()
 export class UUIDv7ValidationPipe implements PipeTransform<string>{
+  private uuidv7Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
   transform(value: string): string {
     if (!value || typeof value !== 'string') {
       throw new BadRequestException(BAD_REQUEST_MESSAGE);
     }
 
-    const uuidv7Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-    return uuidv7Regex.test(value) ? value : '';
+    return this.uuidv7Regex.test(value) ? value : '';
   }
 }
--- src/common/pipe/uuidv7OrEmpty.pipe.ts
+++ src/common/pipe/uuidv7OrEmpty.pipe.ts
@@ -2,12 +2,13 @@ import { Injectable, PipeTransform } from "@nestjs/common";
 
 @Injectable()
 export class UUIDv7OrEmptyPipe implements PipeTransform<string> {
+  private uuidv7Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
   transform(value: string): string {
     if (!value || typeof value !== 'string') {
       return '';
     }
 
-    const uuidv7Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-    return uuidv7Regex.test(value) ? value : '';
+    return this.uuidv7Regex.test(value) ? value : '';
   }
 }
