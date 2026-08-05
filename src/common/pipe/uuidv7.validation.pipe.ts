6972645808537799af3ff080ddc2481cda7ec37f
melonMochiRoll <soulhwasin@gmail.com>
2026-08-05 14:58:57 +0900

    :sparkles: uuidv7가 아니면 에러를 던지도록해 검증 역할을 강화

--- src/common/pipe/uuidv7.validation.pipe.ts
+++ src/common/pipe/uuidv7.validation.pipe.ts
@@ -6,10 +6,10 @@ export class UUIDv7ValidationPipe implements PipeTransform<string>{
   private uuidv7Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
   transform(value: string): string {
-    if (!value || typeof value !== 'string') {
+    if (!value || typeof value !== 'string' || !this.uuidv7Regex.test(value)) {
       throw new BadRequestException(BAD_REQUEST_MESSAGE);
     }
 
-    return this.uuidv7Regex.test(value) ? value : '';
+    return value;
   }
 }
