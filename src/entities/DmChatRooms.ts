c1d856d2443aa166855b85ee810b47bac8a687cf
melonMochiRoll <soulhwasin@gmail.com>
2026-08-27 14:00:44 +0900

    :sparkles: previewUserIds 컬럼 추가

--- src/entities/DmChatRooms.ts
+++ src/entities/DmChatRooms.ts
@@ -1,6 +1,7 @@
 import { UUIDV7Transformer } from "src/common/transformer/uuidv7Transformer";
 import { Column, Entity, Index, JoinColumn, OneToOne, PrimaryColumn } from "typeorm";
 import { ChatRooms } from "./ChatRooms";
+import { ArrayTransformer } from "src/common/transformer/arrayTransformer";
 
 @Index('dm_chatrooms_lastMessageAt_idx', ['lastMessageAt'])
 @Entity({ name: 'dm_chatrooms' })
@@ -13,6 +14,9 @@ export class DmChatRooms {
 
   @Column({ type: 'datetime', name: 'lastMessageAt', precision: 6 })
   lastMessageAt: Date;
+
+  @Column({ type: 'varchar', length: 255, transformer: new ArrayTransformer() })
+  previewUserIds: string[];
 
   @OneToOne(() => ChatRooms, chatRooms => chatRooms.DmChatRoom)
   @JoinColumn({
