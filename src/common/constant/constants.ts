9aa462296397e65b27d73ba4f82f6d540f14ea3e
melonMochiRoll <soulhwasin@gmail.com>
2026-09-10 14:25:57 +0900

    :fire: 더이상 사용하지 않는 상수 제거

--- src/common/constant/constants.ts
+++ src/common/constant/constants.ts
@@ -7,11 +7,6 @@ export const USER_PROVIDER = {
   GOOGLE: 'google',
 } as const;
 
-export const SPACE_TYPE = {
-  SHARED: 'shared',
-  CHAT: 'chat',
-};
-
 export const CHATROOM_TYPE = {
   DM: 'dm',
   SPACE: 'space',
